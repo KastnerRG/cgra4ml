bf9efd0989ef3d2c5803c5f59e137e6e75ef2a61
aba <abarajithan07@gmail.com>
2020-07-23 08:56:15 +0530

    Refactor pad_filter.sv, Change indexing to kw2 based to prevent combinational adder, Fix bug in end_partial

--- src/pad_filter.sv
+++ src/pad_filter.sv
@@ -3,7 +3,7 @@ Group : ABruTECH
 Engineer: Abarajithan G.
 
 Create Date: 21/07/2020
-Design Name: Pad mask
+Design Name: Pad filter
 Tool Versions: Vivado 2018.2
 Description: Computes two masks that allow horizontal zero padding for
                 convolutions where output image should have same size as input.
@@ -17,6 +17,15 @@ Description: Computes two masks that allow horizontal zero padding for
 Revision:
 Revision 0.01 - File Created
 Additional Comments: 
+            OPTIMIZATION
+                - The whiteboard, excel sheet and previous commit (4f4cd) calculates kw2_wire-1,
+                    indexing of end_col, packed dimension of lookup tables and equations
+                    all use (r = kw2-1) based indexing.
+                - To prevent the tool from synthesising a combinational adder to compute
+                    kw2_wire-1, all indexing have been moved up by 1.
+                - indexing of start, end registers begin at 1 and go upto KW2_MAX
+                - For loops begin at 1 and go upto KW2_MAX
+                - In equations, r (=kw2-1) has be replaced by kw2 (=r+1)
 
 //////////////////////////////////////////////////////////////////////////////////*/
 
@@ -41,7 +50,7 @@ module pad_filter # (
     mask_partial,
     mask_full
 );
-    genvar i,k,r;
+    genvar i,k,kw2;
     localparam KW2_MAX           = KERNEL_W_MAX/2; //R, 3->1, 5->2, 7->3
     localparam KERNEL_W_WIDTH    = $clog2(KERNEL_W_MAX   + 1);
 
@@ -55,7 +64,7 @@ module pad_filter # (
     input  wire                      valid_last [KERNEL_W_MAX - 1 : 0];
     input  wire [TUSER_WIDTH - 1: 0] user       [KERNEL_W_MAX - 1 : 0];
 
-    output wire                      mask_partial  [KERNEL_W_MAX - 1 : 1];
+    output wire                      mask_partial [KERNEL_W_MAX - 1 : 1];
     output wire                      mask_full    [KERNEL_W_MAX - 1 : 0];
 
     /*
@@ -70,10 +79,10 @@ module pad_filter # (
     
     */
 
-    wire   [KERNEL_W_WIDTH-1 : 0]    kw2_1_reg_in;
-    wire   [KERNEL_W_WIDTH-1 : 0]    kw2_1_reg_out;
+    wire   [KERNEL_W_WIDTH-1 : 0]    kw2_in;
+    wire   [KERNEL_W_WIDTH-1 : 0]    kw2_wire;
 
-    assign kw2_1_reg_in = kernel_w_1_in/2 -1;
+    assign kw2_in = kernel_w_1_in/2; // kw = 7 : kw2_wire = 3,   kw = 5 : kw2_wire = 2,   kw = 3 : kw2_wire = 1
 
     register
     #(
@@ -85,44 +94,47 @@ module pad_filter # (
         .clock          (aclk),
         .clock_enable   (start),
         .resetn         (aresetn),
-        .data_in        (kw2_1_reg_in),
-        .data_out       (kw2_1_reg_out)
+        .data_in        (kw2_in),
+        .data_out       (kw2_wire)
     );
 
     /*
     COL_START, COL_END Registers
 
-    * One bit
+    * One bit, KW2_MAX regs are there (packed dimension) for every KW_MAX datapath (unpacked dimension)
     * is_col_1_k2 from TUSER, which rises at col==(col-1-k/2) is passed through these
     * updated at each valid_last (end of channels_in)
+    * OPTIMIZATION: 
+        To avoid synthesis of a combinational added when calculating kw/2-1 for indexing,
+        start, end regs are indexed from 1,2...KW2_MAX and indexed by kw2_wire directly.        
 
     * For KW_MAX = 7 kw = 5, signals are asserted in following sequence:
-        - col==(col-3)  :                   end_in[0]
-        - col==(col-2)  :   end_out[0],     end_in[1]
-        - col==(col-1)  :   end_out[1],   start_in[0]  (passed to start_in[0] through a mux)
-        - col==  0      : start_out[0],   start_in[1]
-        - col==  1      : start_out[1],   start_in[2]
+        - col==(col-3)  :                   end_in[1]
+        - col==(col-2)  :   end_out[1],     end_in[2]
+        - col==(col-1)  :   end_out[2],   start_in[1]  (passed to start_in[1] through a mux)
+        - col==  0      : start_out[1],   start_in[2]
+        - col==  1      : start_out[2],   start_in[3]
 
-    *  LAST_COLUMN      :   end_out[kw2_1] asserted
-    * FIRST_COLUMN      : start_out[  0  ] asserted
+    *  LAST_COLUMN      :   end_out[kw2_wire] asserted
+    * FIRST_COLUMN      : start_out[ 1      ] asserted
     
     */
 
 
     wire                            reg_clken           [KERNEL_W_MAX - 1 : 0];
-    wire   [KW2_MAX      - 1 : 0]   col_end_in          [KERNEL_W_MAX - 1 : 0];
-    wire   [KW2_MAX      - 1 : 0]   col_end             [KERNEL_W_MAX - 1 : 0];
-    wire   [KW2_MAX      - 1 : 0]   col_start_in        [KERNEL_W_MAX - 1 : 0];
-    wire   [KW2_MAX      - 1 : 0]   col_start           [KERNEL_W_MAX - 1 : 0];
+    wire   [KW2_MAX          : 1]   col_end_in          [KERNEL_W_MAX - 1 : 0];
+    wire   [KW2_MAX          : 1]   col_end             [KERNEL_W_MAX - 1 : 0];
+    wire   [KW2_MAX          : 1]   col_start_in        [KERNEL_W_MAX - 1 : 0];
+    wire   [KW2_MAX          : 1]   col_start           [KERNEL_W_MAX - 1 : 0];
     generate
         for (i=0; i < KERNEL_W_MAX; i = i+1) begin: col_end_gen_i
 
             assign reg_clken[i] = valid_last  [i] && aclken;
 
-            assign col_end_in         [i][0]  = user[i][INDEX_IS_COLS_1_K2];
-            assign col_start_in       [i][0]  = col_end[i][kw2_1_reg_out]; // This is a mux
+            assign col_end_in         [i][1]  = user[i][INDEX_IS_COLS_1_K2];
+            assign col_start_in       [i][1]  = col_end[i][kw2_wire]; // This is a mux
 
-            for (   k=1;    k < KW2_MAX;    k = k+1) begin: col_end_gen_k
+            for (   k=2;    k < KW2_MAX+1;    k = k+1) begin: col_end_gen_k
                 assign col_end_in     [i][k]  = col_end[i][k-1];
                 assign col_start_in   [i][k]  = col_start[i][k-1];
             end
@@ -164,49 +176,56 @@ module pad_filter # (
         independantly for datapaths, allowing back to back kernel change
     * table[datapath][kernel]
     * logic is explained. Refer architecture.xlsx or whiteboard for further details
+
+    * OPTIMIZATION: 
+        - To avoid synthesis of a combinational added when calculating kw/2-1 for indexing,
+            kernel dimension of LUTs go 1,2,...,KW2_MAX and indexed by kw_wire directly
+        - All equations are transformed in terms of kw2
+        - NOTE: Excel sheet and whiteboard are in terms of (r = kw2-1)
+
     */
     
-    bit   lut_allow_full     [KERNEL_W_MAX - 1 : 0] [KW2_MAX      - 1 : 0];
-    bit   lut_stop_partial   [KERNEL_W_MAX - 1 : 0] [KW2_MAX      - 1 : 0]; 
+    bit   lut_allow_full     [KERNEL_W_MAX - 1 : 0] [KW2_MAX : 1];
+    bit   lut_stop_partial   [KERNEL_W_MAX - 1 : 0] [KW2_MAX : 1]; 
 
     wire   is_1x1 = user[INDEX_IS_1x1];
     generate
         for ( i=0; i < KERNEL_W_MAX; i = i+1)   begin: lookup_full_datapath_gen
-            for ( r=0;  r < KW2_MAX; r = r+1)   begin: lookup_full_kw_gen
+            for ( kw2=1;  kw2 < KW2_MAX+1; kw2 = kw2+1)   begin: lookup_full_kw_gen
             
-                wire full_datapath             =     i == 2*(r+1)        ; // i == kw-1 = (2k2+1)-1 = (2(r+1)+1)-1 = 2r+3-1 = 2(r+2)
-                wire unused_datapaths          =     i >  2*(r+1)        ; // Anything above that is unused
+                wire full_datapath             =     i == 2*kw2            ; // i == kw-1 = (2k2+1)-1 = (2(kw2)+1)-1 = 2kw2
+                wire unused_datapaths          =     i >  2*kw2            ; // Anything above that is unused
 
-                wire start_cols                =     |col_start[i][r:0]  ; // 0,1,...k2-1 : first k/2 colums are to be ignored
-                wire last_col                  =      col_end  [i][r  ]  ; // if the last column...
+                wire start_cols                =     |col_start[i][kw2:1]  ; // 1,2,...k2 : first k/2 colums are to be ignored
+                wire last_col                  =      col_end  [i][kw2  ]  ; // if the last column:
 
-                wire last_partial              =     i >  r              ; // i > k2-1 : i >= k2 : All (i<k2) datapaths contain insuffient data
+                wire last_malformed            =     i <  kw2              ; // All (i<k2) datapaths contain malformed data, rest contain padded data
 
                 wire at_start_and_middle       =     full_datapath & !start_cols; // During start_cols, block all datapaths. During middle_cols, allow only full_datapth.
-                wire at_last_col               =     last_col & last_partial & !unused_datapaths; // At the last_col, only allow datapaths that have partially formed padding
+                wire at_last_col               =     last_col & !last_malformed & !unused_datapaths; // At the last_col, only allow datapaths that have partially formed padding
 
-                assign lut_allow_full   [i][r] =     at_start_and_middle | at_last_col | is_1x1;
+                assign lut_allow_full   [i][kw2] =     at_start_and_middle | at_last_col | is_1x1;
             end
 
-            assign    mask_full[i]             =     lut_allow_full    [i][kw2_1_reg_out];
+            assign    mask_full[i]             =     lut_allow_full    [i][kw2_wire];
         end
 
         for ( i=1; i < KERNEL_W_MAX; i = i+1)   begin: lookup_partial_datapath_gen
-            for ( r=0;  r <  KW2_MAX ; r = r+1) begin: lut_partial_kw_gen
+            for ( kw2=1;  kw2 <  KW2_MAX+1 ; kw2 = kw2+1) begin: lut_partial_kw_gen
 
-                wire unused_datapaths  =  i >  2*(r+1) ;        // Anything above i == kw-1 should be blocked
+                wire unused_datapaths  =  i >  2*kw2 ;          // Anything above i == kw2 should be blocked
                 wire end_partial;
 
-                if (r < i-1)
-                    assign end_partial = 1;
+                if (i > kw2)
+                    assign end_partial = 0;                     // Dont block i>kw2 datapaths. We need those partial sums for first few columns
                 else
-                    assign end_partial = |col_end[i][r:i-1];    // or(i-1, i, i+1, ... k2-1), horizontal rows of the blocking triangle
+                    assign end_partial = |col_end[i][kw2:i];    // or(i, i+1, i+2, ... k2), horizontal rows of the blocking triangle
 
-               assign lut_stop_partial [i][r] =  end_partial | unused_datapaths;
+               assign lut_stop_partial [i][kw2] =  end_partial | unused_datapaths;
                 
             end
 
-            assign    mask_partial[i]         = !lut_stop_partial [i][kw2_1_reg_out];
+            assign    mask_partial[i]         = !lut_stop_partial [i][kw2_wire];
         end
 
     endgenerate
