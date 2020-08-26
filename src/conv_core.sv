1749f7aabd6cbee96c525fa0b4072393cb76a556
aba <abarajithan07@gmail.com>
2020-08-26 12:03:29 +0530

    Refactor

--- src/conv_core.sv
+++ src/conv_core.sv
@@ -1,3 +1,22 @@
+/*//////////////////////////////////////////////////////////////////////////////////
+Group : ABruTECH
+Engineer: Abarajithan G.
+
+Create Date: 20/08/2020
+Design Name: AXIS Convolution Core
+Tool Versions: Vivado 2018.2
+Description:
+            * One core computes one output filter.
+            * Weights are stepped inside here.
+            * Generates CONV_UNITS number of units, sets the first one ACTIVE and 
+                provides the control signals driven by it to PASSIVE ones.
+
+Revision:
+Revision 0.01 - File Created
+Additional Comments: 
+
+//////////////////////////////////////////////////////////////////////////////////*/
+
 module conv_core # (
     parameter IS_FIXED_POINT         ,
     parameter CONV_UNITS             ,
@@ -35,16 +54,16 @@ module conv_core # (
     m_user              
 
 );
-    localparam KERNEL_W_WIDTH       = $clog2(KERNEL_W_MAX   + 1);
+    localparam KERNEL_W_WIDTH       = $clog2(KERNEL_W_MAX   + 1)                                      ;
     localparam ACCUMULATOR_DELAY    = IS_FIXED_POINT ? FIXED_ACCUMULATOR_DELAY : FLOAT_ACCUMULATOR_DELAY ; 
 
-    input  wire                      aclk;
-    input  wire                      aclken;               
-    input  wire                      aresetn;
+    input  wire                      aclk                                                             ;
+    input  wire                      aclken                                                           ;               
+    input  wire                      aresetn                                                          ;
 
-    input  wire                      start;
-    input  wire [KERNEL_W_WIDTH-1:0] kernel_w_1;
-    input  wire                      is_1x1;
+    input  wire                      start                                                            ;
+    input  wire [KERNEL_W_WIDTH-1:0] kernel_w_1                                                       ;
+    input  wire                      is_1x1                                                           ;
 
     output wire                      s_ready                                                          ;
     input  wire                      s_weights_valid                                                  ;
@@ -54,44 +73,46 @@ module conv_core # (
     input  wire                      s_step_pixels_last                         [KERNEL_W_MAX - 1 : 0];
     input  wire [TUSER_WIDTH - 1: 0] s_step_pixels_user                         [KERNEL_W_MAX - 1 : 0];
 
-    output wire                      m_valid                                    ;
-    output wire [DATA_WIDTH  - 1: 0] m_data               [CONV_UNITS  - 1  : 0];
-    output wire                      m_last                                     ;
-    output wire [TUSER_WIDTH - 1: 0] m_user                                     ;
+    output wire                      m_valid                                                          ;
+    output wire [DATA_WIDTH  - 1: 0] m_data               [CONV_UNITS  - 1  : 0]                      ;
+    output wire                      m_last                                                           ;
+    output wire [TUSER_WIDTH - 1: 0] m_user                                                           ;
 
     /*
         STEP BUFFER FOR WEIGHTS
     */
 
-    assign s_ready  = conv_s_ready [0];
-
     wire                      m_step_weights_valid [KERNEL_W_MAX - 1 : 0];
     wire [DATA_WIDTH  - 1: 0] m_step_weights_data  [KERNEL_W_MAX - 1 : 0];
 
     step_buffer  #(
-        .DATA_WIDTH       (DATA_WIDTH),
-        .STEPS            (KERNEL_W_MAX),
-        .ACCUMULATOR_DELAY(ACCUMULATOR_DELAY),
-        .TUSER_WIDTH      (TUSER_WIDTH)
+        .DATA_WIDTH       (DATA_WIDTH                   ),
+        .STEPS            (KERNEL_W_MAX                 ),
+        .ACCUMULATOR_DELAY(ACCUMULATOR_DELAY            ),
+        .TUSER_WIDTH      (TUSER_WIDTH                  )
     )
     step_buffer_weights
     (
-        .aclk       (aclk                            ),
-        .aclken     (s_ready                         ), // clken_mul
-        .aresetn    (aresetn                         ),
-        .is_1x1     (is_1x1                          ),
+        .aclk       (aclk                               ),
+        .aclken     (s_ready                            ),
+        .aresetn    (aresetn                            ),
+        .is_1x1     (is_1x1                             ),
         
-        .s_valid    ('{KERNEL_W_MAX{s_weights_valid}}),
-        .s_data     (s_weights_data                  ),
+        .s_valid    ('{KERNEL_W_MAX{s_weights_valid}}   ),
+        .s_data     (s_weights_data                     ),
 
-        .m_valid    (m_step_weights_valid            ),
-        .m_data     (m_step_weights_data             )
+        .m_valid    (m_step_weights_valid               ),
+        .m_data     (m_step_weights_data                )
     );
 
-    wire                   conv_s_ready [CONV_UNITS-1 : 0];
-    wire                   conv_m_valid [CONV_UNITS-1 : 0];
-    wire                   conv_m_last  [CONV_UNITS-1 : 0];
-    wire [TUSER_WIDTH-1:0] conv_m_user  [CONV_UNITS-1 : 0];
+    
+    /*
+        - CONV_UNITS number of units are generated.
+        - First unit is set ACTIVE, rest are set PASSIVE
+        - s_ready, m_valid, m_user, m_last come from ACTIVE unit
+    */
+
+    // Wires that distribute driven signals from out_* of ACTIVE to in_* of passive units.
 
     wire [KERNEL_W_MAX-1 : 1] mux_sel                           ;
     wire                      clken_mul                         ;
@@ -101,100 +122,85 @@ module conv_core # (
     wire                      acc_s_last  [KERNEL_W_MAX - 1 : 0];     
     wire                      shift_sel   [KERNEL_W_MAX - 2 : 0];     
 
-    assign m_valid = conv_m_valid [0];
-    assign m_last  = conv_m_last  [0];
-    assign m_user  = conv_m_user  [0];
-    
     genvar i;
     generate
-    for (i=0; i < CONV_UNITS; i++) begin: conv_unit_gen
-        if (i == 0)
-            conv_unit # (
-                    .IS_ACTIVE                (1),
-                    .IS_FIXED_POINT           (IS_FIXED_POINT),
-                    .DATA_WIDTH               (DATA_WIDTH),
-                    .KERNEL_W_MAX             (KERNEL_W_MAX),
-                    .TUSER_WIDTH              (TUSER_WIDTH),
-                    .FIXED_ACCUMULATOR_DELAY  (FIXED_ACCUMULATOR_DELAY),
-                    .FIXED_MULTIPLIER_DELAY   (FIXED_MULTIPLIER_DELAY),
-                    .INDEX_IS_1x1             (INDEX_IS_1x1),
-                    .INDEX_IS_MAX             (INDEX_IS_MAX),
-                    .INDEX_IS_RELU            (INDEX_IS_RELU),
-                    .INDEX_IS_COLS_1_K2       (INDEX_IS_COLS_1_K2)
-                )
-                CONV_UNIT
-                (
-                    .aclk               (aclk                     ),
-                    .aresetn            (aresetn                  ),
-                    .s_step_pixels_data (s_step_pixels_data   [i] ),
-                    .s_step_weights_valid(m_step_weights_valid    ),
-                    .s_step_weights_data(m_step_weights_data      ),
-                    .m_data             (m_data               [i] ),
+        for (i=0; i < CONV_UNITS; i++) begin: conv_unit_gen
+            if (i == 0)
+                conv_unit # (
+                        .IS_ACTIVE                (1                      ),
+                        .IS_FIXED_POINT           (IS_FIXED_POINT         ),
+                        .DATA_WIDTH               (DATA_WIDTH             ),
+                        .KERNEL_W_MAX             (KERNEL_W_MAX           ),
+                        .TUSER_WIDTH              (TUSER_WIDTH            ),
+                        .FIXED_ACCUMULATOR_DELAY  (FIXED_ACCUMULATOR_DELAY),
+                        .FIXED_MULTIPLIER_DELAY   (FIXED_MULTIPLIER_DELAY ),
+                        .INDEX_IS_1x1             (INDEX_IS_1x1           ),
+                        .INDEX_IS_MAX             (INDEX_IS_MAX           ),
+                        .INDEX_IS_RELU            (INDEX_IS_RELU          ),
+                        .INDEX_IS_COLS_1_K2       (INDEX_IS_COLS_1_K2     )
+                    )
+                    CONV_UNIT
+                    (
+                        .aclk                   (aclk                     ),
+                        .aclken                 (aclken                   ),
+                        .aresetn                (aresetn                  ),
+                        .s_step_pixels_data     (s_step_pixels_data   [i] ),
+                        .s_step_weights_valid   (m_step_weights_valid     ),
+                        .s_step_weights_data    (m_step_weights_data      ),
+                        .m_data                 (m_data               [i] ),
 
-                    .aclken             (aclken                   ),
-                    .start              (start                    ),
-                    .kernel_w_1         (kernel_w_1               ),
-                    .s_ready            (conv_s_ready         [i] ),
-                    
-                    .s_step_pixels_last (s_step_pixels_last       ),
-                    .s_step_pixels_user (s_step_pixels_user       ),
+                        .start                  (start                    ),
+                        .kernel_w_1             (kernel_w_1               ),
 
-                    .m_valid            (conv_m_valid         [i] ),
-                    .m_last             (conv_m_last          [i] ),
-                    .m_user             (conv_m_user          [i] ),
+                        .s_step_pixels_last     (s_step_pixels_last       ),
+                        .s_step_pixels_user     (s_step_pixels_user       ),
 
-                    .out_mux_sel         (mux_sel             ),
-                    .out_clken_mul       (clken_mul           ),
-                    .out_clken_acc       (clken_acc           ),
-                    .out_mux_s2_valid    (mux_s2_valid        ),
-                    .out_acc_s_valid     (acc_s_valid         ),
-                    .out_acc_s_last      (acc_s_last          ),
-                    .out_shift_sel       (shift_sel           )
+                        .s_ready                (s_ready                  ),
+                        .m_valid                (m_valid                  ),
+                        .m_last                 (m_last                   ),
+                        .m_user                 (m_user                   ),
 
-                );
-        else
-            conv_unit # (
-                    .IS_ACTIVE                (0),
-                    .IS_FIXED_POINT           (IS_FIXED_POINT),
-                    .DATA_WIDTH               (DATA_WIDTH),
-                    .KERNEL_W_MAX             (KERNEL_W_MAX),
-                    .TUSER_WIDTH              (TUSER_WIDTH),
-                    .FIXED_ACCUMULATOR_DELAY  (FIXED_ACCUMULATOR_DELAY),
-                    .FIXED_MULTIPLIER_DELAY   (FIXED_MULTIPLIER_DELAY),
-                    .INDEX_IS_1x1             (INDEX_IS_1x1),
-                    .INDEX_IS_MAX             (INDEX_IS_MAX),
-                    .INDEX_IS_RELU            (INDEX_IS_RELU),
-                    .INDEX_IS_COLS_1_K2       (INDEX_IS_COLS_1_K2)
-                )
-                CONV_UNIT
-                (
-                    .aclk               (aclk                     ),
-                    .aresetn            (aresetn                  ),
-                    .s_step_pixels_data (s_step_pixels_data   [i] ),
-                    .s_step_weights_valid(m_step_weights_valid    ),
-                    .s_step_weights_data(m_step_weights_data      ),
-                    .m_data             (m_data               [i] ),
+                        .out_mux_sel            (mux_sel                  ),
+                        .out_clken_mul          (clken_mul                ),
+                        .out_clken_acc          (clken_acc                ),
+                        .out_mux_s2_valid       (mux_s2_valid             ),
+                        .out_acc_s_valid        (acc_s_valid              ),
+                        .out_acc_s_last         (acc_s_last               ),
+                        .out_shift_sel          (shift_sel                )
 
-                    .aclken             (aclken                   ),
-                    .start              (start                    ),
-                    .kernel_w_1         (kernel_w_1               ),
-                    .s_ready            (conv_s_ready         [i] ),
-                    
-                    .s_step_pixels_last (s_step_pixels_last       ),
-                    .s_step_pixels_user (s_step_pixels_user       ),
+                    );
+            else
+                conv_unit # (
+                        .IS_ACTIVE                (0                      ),
+                        .IS_FIXED_POINT           (IS_FIXED_POINT         ),
+                        .DATA_WIDTH               (DATA_WIDTH             ),
+                        .KERNEL_W_MAX             (KERNEL_W_MAX           ),
+                        .TUSER_WIDTH              (TUSER_WIDTH            ),
+                        .FIXED_ACCUMULATOR_DELAY  (FIXED_ACCUMULATOR_DELAY),
+                        .FIXED_MULTIPLIER_DELAY   (FIXED_MULTIPLIER_DELAY ),
+                        .INDEX_IS_1x1             (INDEX_IS_1x1           ),
+                        .INDEX_IS_MAX             (INDEX_IS_MAX           ),
+                        .INDEX_IS_RELU            (INDEX_IS_RELU          ),
+                        .INDEX_IS_COLS_1_K2       (INDEX_IS_COLS_1_K2     )
+                    )
+                    CONV_UNIT
+                    (
+                        .aclk                   (aclk                     ),
+                        .aclken                 (aclken                   ),
+                        .aresetn                (aresetn                  ),
+                        .s_step_pixels_data     (s_step_pixels_data   [i] ),
+                        .s_step_weights_valid   (m_step_weights_valid     ),
+                        .s_step_weights_data    (m_step_weights_data      ),
+                        .m_data                 (m_data               [i] ),
 
-                    .m_valid            (conv_m_valid         [i] ),
-                    .m_last             (conv_m_last          [i] ),
-                    .m_user             (conv_m_user          [i] ),
-
-                    .in_mux_sel          (mux_sel             ),
-                    .in_clken_mul        (clken_mul           ),
-                    .in_clken_acc        (clken_acc           ),
-                    .in_mux_s2_valid     (mux_s2_valid        ),
-                    .in_acc_s_valid      (acc_s_valid         ),
-                    .in_acc_s_last       (acc_s_last          ),
-                    .in_shift_sel        (shift_sel           )
-                );
+                        .in_mux_sel             (mux_sel                  ),
+                        .in_clken_mul           (clken_mul                ),
+                        .in_clken_acc           (clken_acc                ),
+                        .in_mux_s2_valid        (mux_s2_valid             ),
+                        .in_acc_s_valid         (acc_s_valid              ),
+                        .in_acc_s_last          (acc_s_last               ),
+                        .in_shift_sel           (shift_sel                )
+                    );
         end
     endgenerate
 
