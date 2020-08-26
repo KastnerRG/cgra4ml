0a369167f86df0d685dd803dbab52c5bff21ec13
aba <abarajithan07@gmail.com>
2020-08-26 10:55:59 +0530

    Make one conv_unit in each core ACTIVE (controller) and other units PASSIVE. Verified: float, fixed, wires are all connected.

--- projects/unit_tests/build_support.tcl
+++ projects/unit_tests/build_support.tcl
@@ -17,15 +17,25 @@ update_ip_catalog
 
 #--------------- Generate IPs
 
-# Float multiplier (6 clocks)
-create_ip -name floating_point -vendor xilinx.com -library ip -version 7.1 -module_name floating_point_multiplier
-set_property -dict [list CONFIG.Component_Name {floating_point_multiplier} CONFIG.Operation_Type {Multiply} CONFIG.A_Precision_Type {Half} CONFIG.Flow_Control {NonBlocking} CONFIG.Has_ACLKEN {true} CONFIG.Has_ARESETn {true} CONFIG.Has_A_TLAST {true} CONFIG.Has_A_TUSER {true} CONFIG.A_TUSER_Width {4} CONFIG.C_A_Exponent_Width {5} CONFIG.C_A_Fraction_Width {11} CONFIG.Result_Precision_Type {Half} CONFIG.C_Result_Exponent_Width {5} CONFIG.C_Result_Fraction_Width {11} CONFIG.C_Accum_Msb {32} CONFIG.C_Accum_Lsb {-24} CONFIG.C_Accum_Input_Msb {15} CONFIG.C_Mult_Usage {Full_Usage} CONFIG.Has_RESULT_TREADY {false} CONFIG.C_Latency {6} CONFIG.C_Rate {1} CONFIG.RESULT_TLAST_Behv {Pass_A_TLAST}] [get_ips floating_point_multiplier]
-generate_target {instantiation_template} [get_files ./$PROJ_FOLDER/$PROJ_NAME.srcs/sources_1/ip/floating_point_multiplier/floating_point_multiplier.xci]
+# Active Float multiplier (6 clocks)
+create_ip -name floating_point -vendor xilinx.com -library ip -version 7.1 -module_name floating_point_multiplier_active
+set_property -dict [list CONFIG.Component_Name {floating_point_multiplier_active} CONFIG.Operation_Type {Multiply} CONFIG.A_Precision_Type {Half} CONFIG.Flow_Control {NonBlocking} CONFIG.Has_ACLKEN {true} CONFIG.Has_ARESETn {true} CONFIG.Has_A_TLAST {true} CONFIG.Has_A_TUSER {true} CONFIG.A_TUSER_Width {4} CONFIG.C_A_Exponent_Width {5} CONFIG.C_A_Fraction_Width {11} CONFIG.Result_Precision_Type {Half} CONFIG.C_Result_Exponent_Width {5} CONFIG.C_Result_Fraction_Width {11} CONFIG.C_Accum_Msb {32} CONFIG.C_Accum_Lsb {-24} CONFIG.C_Accum_Input_Msb {15} CONFIG.C_Mult_Usage {Full_Usage} CONFIG.Has_RESULT_TREADY {false} CONFIG.C_Latency {6} CONFIG.C_Rate {1} CONFIG.RESULT_TLAST_Behv {Pass_A_TLAST}] [get_ips floating_point_multiplier_active]
+generate_target {instantiation_template} [get_files ./$PROJ_FOLDER/$PROJ_NAME.srcs/sources_1/ip/floating_point_multiplier_active/floating_point_multiplier_active.xci]
 
-# Float Accumulator (19----- clocks)
-create_ip -name floating_point -vendor xilinx.com -library ip -version 7.1 -module_name floating_point_accumulator
-set_property -dict [list CONFIG.Component_Name {floating_point_accumulator} CONFIG.Operation_Type {Accumulator} CONFIG.Add_Sub_Value {Add} CONFIG.A_Precision_Type {Half} CONFIG.C_Mult_Usage {No_Usage} CONFIG.Flow_Control {NonBlocking} CONFIG.Has_ACLKEN {true} CONFIG.Has_ARESETn {true} CONFIG.Has_A_TUSER {true} CONFIG.A_TUSER_Width {4} CONFIG.C_A_Exponent_Width {5} CONFIG.C_A_Fraction_Width {11} CONFIG.Result_Precision_Type {Half} CONFIG.C_Result_Exponent_Width {5} CONFIG.C_Result_Fraction_Width {11} CONFIG.C_Accum_Msb {32} CONFIG.C_Accum_Lsb {-24} CONFIG.C_Accum_Input_Msb {15} CONFIG.Has_RESULT_TREADY {false} CONFIG.C_Latency {19} CONFIG.C_Rate {1} CONFIG.Has_A_TLAST {true} CONFIG.RESULT_TLAST_Behv {Pass_A_TLAST}] [get_ips floating_point_accumulator]
-generate_target {instantiation_template} [get_files ./$PROJ_FOLDER/$PROJ_NAME.srcs/sources_1/ip/floating_point_accumulator/floating_point_accumulator.xci]
+# Acive Float Accumulator (19----- clocks)
+create_ip -name floating_point -vendor xilinx.com -library ip -version 7.1 -module_name floating_point_accumulator_active
+set_property -dict [list CONFIG.Component_Name {floating_point_accumulator_active} CONFIG.Operation_Type {Accumulator} CONFIG.Add_Sub_Value {Add} CONFIG.A_Precision_Type {Half} CONFIG.C_Mult_Usage {No_Usage} CONFIG.Flow_Control {NonBlocking} CONFIG.Has_ACLKEN {true} CONFIG.Has_ARESETn {true} CONFIG.Has_A_TUSER {true} CONFIG.A_TUSER_Width {4} CONFIG.C_A_Exponent_Width {5} CONFIG.C_A_Fraction_Width {11} CONFIG.Result_Precision_Type {Half} CONFIG.C_Result_Exponent_Width {5} CONFIG.C_Result_Fraction_Width {11} CONFIG.C_Accum_Msb {32} CONFIG.C_Accum_Lsb {-24} CONFIG.C_Accum_Input_Msb {15} CONFIG.Has_RESULT_TREADY {false} CONFIG.C_Latency {19} CONFIG.C_Rate {1} CONFIG.Has_A_TLAST {true} CONFIG.RESULT_TLAST_Behv {Pass_A_TLAST}] [get_ips floating_point_accumulator_active]
+generate_target {instantiation_template} [get_files ./$PROJ_FOLDER/$PROJ_NAME.srcs/sources_1/ip/floating_point_accumulator_active/floating_point_accumulator_active.xci]
+
+# Passive Float multiplier (6 clocks)
+create_ip -name floating_point -vendor xilinx.com -library ip -version 7.1 -module_name floating_point_multiplier_passive
+set_property -dict [list CONFIG.Component_Name {floating_point_multiplier_passive} CONFIG.Operation_Type {Multiply} CONFIG.A_Precision_Type {Half} CONFIG.C_Mult_Usage {No_Usage} CONFIG.Flow_Control {NonBlocking} CONFIG.Has_ACLKEN {true} CONFIG.Has_ARESETn {true} CONFIG.C_A_Exponent_Width {5} CONFIG.C_A_Fraction_Width {11} CONFIG.Result_Precision_Type {Half} CONFIG.C_Result_Exponent_Width {5} CONFIG.C_Result_Fraction_Width {11} CONFIG.C_Accum_Msb {32} CONFIG.C_Accum_Lsb {-24} CONFIG.C_Accum_Input_Msb {15} CONFIG.C_Mult_Usage {No_Usage} CONFIG.Has_RESULT_TREADY {false} CONFIG.C_Latency {6} CONFIG.C_Rate {1}] [get_ips floating_point_multiplier_passive]
+generate_target {instantiation_template} [get_files ./$PROJ_FOLDER/$PROJ_NAME.srcs/sources_1/ip/floating_point_multiplier_passive/floating_point_multiplier_passive.xci}}]
+
+# Passive Float Accumulator (19----- clocks)
+create_ip -name floating_point -vendor xilinx.com -library ip -version 7.1 -module_name floating_point_accumulator_passive 
+set_property -dict [list CONFIG.Component_Name {floating_point_accumulator_passive} CONFIG.Operation_Type {Accumulator} CONFIG.Add_Sub_Value {Add} CONFIG.A_Precision_Type {Half} CONFIG.C_Mult_Usage {No_Usage} CONFIG.Flow_Control {NonBlocking} CONFIG.Maximum_Latency {true} CONFIG.Has_ACLKEN {true} CONFIG.Has_ARESETn {true} CONFIG.C_A_Exponent_Width {5} CONFIG.C_A_Fraction_Width {11} CONFIG.Result_Precision_Type {Half} CONFIG.C_Result_Exponent_Width {5} CONFIG.C_Result_Fraction_Width {11} CONFIG.C_Accum_Msb {32} CONFIG.C_Accum_Lsb {-24} CONFIG.C_Accum_Input_Msb {15} CONFIG.Has_RESULT_TREADY {false} CONFIG.C_Latency {19} CONFIG.C_Rate {1} CONFIG.Has_A_TLAST {true} CONFIG.RESULT_TLAST_Behv {Pass_A_TLAST}] [get_ips floating_point_accumulator_passive]
+generate_target {instantiation_template} [get_files ./$PROJ_FOLDER/$PROJ_NAME.srcs/sources_1/ip/floating_point_accumulator_passive/floating_point_accumulator_passive.xci}}]
 
 # Fixed Multiplier (3 clocks - optimum)
 create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name fixed_point_multiplier
--- src/conv_core.sv
+++ src/conv_core.sv
@@ -93,6 +93,14 @@ module conv_core # (
     wire                   conv_m_last  [CONV_UNITS-1 : 0];
     wire [TUSER_WIDTH-1:0] conv_m_user  [CONV_UNITS-1 : 0];
 
+    wire [KERNEL_W_MAX-1 : 1] mux_sel                           ;
+    wire                      clken_mul                         ;
+    wire [KERNEL_W_MAX-1 : 0] clken_acc                         ;
+    wire                      mux_s2_valid[KERNEL_W_MAX - 1 : 1];
+    wire                      acc_s_valid [KERNEL_W_MAX - 1 : 0];
+    wire                      acc_s_last  [KERNEL_W_MAX - 1 : 0];     
+    wire                      shift_sel   [KERNEL_W_MAX - 2 : 0];     
+
     assign m_valid = conv_m_valid [0];
     assign m_last  = conv_m_last  [0];
     assign m_user  = conv_m_user  [0];
@@ -100,40 +108,94 @@ module conv_core # (
     genvar i;
     generate
     for (i=0; i < CONV_UNITS; i++) begin: conv_unit_gen
-            
-       conv_unit # (
-            .IS_FIXED_POINT           (IS_FIXED_POINT),
-            .DATA_WIDTH               (DATA_WIDTH),
-            .KERNEL_W_MAX             (KERNEL_W_MAX),
-            .TUSER_WIDTH              (TUSER_WIDTH),
-            .FIXED_ACCUMULATOR_DELAY  (FIXED_ACCUMULATOR_DELAY),
-            .FIXED_MULTIPLIER_DELAY   (FIXED_MULTIPLIER_DELAY),
-            .INDEX_IS_1x1             (INDEX_IS_1x1),
-            .INDEX_IS_MAX             (INDEX_IS_MAX),
-            .INDEX_IS_RELU            (INDEX_IS_RELU),
-            .INDEX_IS_COLS_1_K2       (INDEX_IS_COLS_1_K2)
-        )
-        CONV_UNIT
-        (
-            .aclk               (aclk                     ),
-            .aclken             (aclken                   ),
-            .aresetn            (aresetn                  ),
-            .start              (start                    ),
-            .kernel_w_1         (kernel_w_1               ),
+        if (i == 0)
+            conv_unit # (
+                    .IS_ACTIVE                (1),
+                    .IS_FIXED_POINT           (IS_FIXED_POINT),
+                    .DATA_WIDTH               (DATA_WIDTH),
+                    .KERNEL_W_MAX             (KERNEL_W_MAX),
+                    .TUSER_WIDTH              (TUSER_WIDTH),
+                    .FIXED_ACCUMULATOR_DELAY  (FIXED_ACCUMULATOR_DELAY),
+                    .FIXED_MULTIPLIER_DELAY   (FIXED_MULTIPLIER_DELAY),
+                    .INDEX_IS_1x1             (INDEX_IS_1x1),
+                    .INDEX_IS_MAX             (INDEX_IS_MAX),
+                    .INDEX_IS_RELU            (INDEX_IS_RELU),
+                    .INDEX_IS_COLS_1_K2       (INDEX_IS_COLS_1_K2)
+                )
+                CONV_UNIT
+                (
+                    .aclk               (aclk                     ),
+                    .aresetn            (aresetn                  ),
+                    .s_step_pixels_data (s_step_pixels_data   [i] ),
+                    .s_step_weights_valid(m_step_weights_valid    ),
+                    .s_step_weights_data(m_step_weights_data      ),
+                    .m_data             (m_data               [i] ),
 
-            .s_ready            (conv_s_ready         [i] ),
-            .s_step_pixels_data (s_step_pixels_data   [i] ),
-            .s_step_weights_valid(m_step_weights_valid    ),
-            .s_step_weights_data(m_step_weights_data      ),
-            .s_step_pixels_last (s_step_pixels_last       ),
-            .s_step_pixels_user (s_step_pixels_user       ),
+                    .aclken             (aclken                   ),
+                    .start              (start                    ),
+                    .kernel_w_1         (kernel_w_1               ),
+                    .s_ready            (conv_s_ready         [i] ),
+                    
+                    .s_step_pixels_last (s_step_pixels_last       ),
+                    .s_step_pixels_user (s_step_pixels_user       ),
 
-            .m_valid            (conv_m_valid         [i] ),
-            .m_data             (m_data               [i] ),
-            .m_last             (conv_m_last          [i] ),
-            .m_user             (conv_m_user          [i] )
-        );
-    end
+                    .m_valid            (conv_m_valid         [i] ),
+                    .m_last             (conv_m_last          [i] ),
+                    .m_user             (conv_m_user          [i] ),
+
+                    .out_mux_sel         (mux_sel             ),
+                    .out_clken_mul       (clken_mul           ),
+                    .out_clken_acc       (clken_acc           ),
+                    .out_mux_s2_valid    (mux_s2_valid        ),
+                    .out_acc_s_valid     (acc_s_valid         ),
+                    .out_acc_s_last      (acc_s_last          ),
+                    .out_shift_sel       (shift_sel           )
+
+                );
+        else
+            conv_unit # (
+                    .IS_ACTIVE                (0),
+                    .IS_FIXED_POINT           (IS_FIXED_POINT),
+                    .DATA_WIDTH               (DATA_WIDTH),
+                    .KERNEL_W_MAX             (KERNEL_W_MAX),
+                    .TUSER_WIDTH              (TUSER_WIDTH),
+                    .FIXED_ACCUMULATOR_DELAY  (FIXED_ACCUMULATOR_DELAY),
+                    .FIXED_MULTIPLIER_DELAY   (FIXED_MULTIPLIER_DELAY),
+                    .INDEX_IS_1x1             (INDEX_IS_1x1),
+                    .INDEX_IS_MAX             (INDEX_IS_MAX),
+                    .INDEX_IS_RELU            (INDEX_IS_RELU),
+                    .INDEX_IS_COLS_1_K2       (INDEX_IS_COLS_1_K2)
+                )
+                CONV_UNIT
+                (
+                    .aclk               (aclk                     ),
+                    .aresetn            (aresetn                  ),
+                    .s_step_pixels_data (s_step_pixels_data   [i] ),
+                    .s_step_weights_valid(m_step_weights_valid    ),
+                    .s_step_weights_data(m_step_weights_data      ),
+                    .m_data             (m_data               [i] ),
+
+                    .aclken             (aclken                   ),
+                    .start              (start                    ),
+                    .kernel_w_1         (kernel_w_1               ),
+                    .s_ready            (conv_s_ready         [i] ),
+                    
+                    .s_step_pixels_last (s_step_pixels_last       ),
+                    .s_step_pixels_user (s_step_pixels_user       ),
+
+                    .m_valid            (conv_m_valid         [i] ),
+                    .m_last             (conv_m_last          [i] ),
+                    .m_user             (conv_m_user          [i] ),
+
+                    .in_mux_sel          (mux_sel             ),
+                    .in_clken_mul        (clken_mul           ),
+                    .in_clken_acc        (clken_acc           ),
+                    .in_mux_s2_valid     (mux_s2_valid        ),
+                    .in_acc_s_valid      (acc_s_valid         ),
+                    .in_acc_s_last       (acc_s_last          ),
+                    .in_shift_sel        (shift_sel           )
+                );
+        end
     endgenerate
 
 
