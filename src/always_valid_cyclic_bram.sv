918562c766980bcd55d13e0c953a0a325549650f
aba <abarajithan07@gmail.com>
2020-12-03 07:14:56 +0530

    Fix for latency=2 and test. Add comments, Refactor

--- src/always_valid_cyclic_bram.sv
+++ src/always_valid_cyclic_bram.sv
@@ -1,3 +1,26 @@
+/*//////////////////////////////////////////////////////////////////////////////////
+Group : ABruTECH
+Engineer: Abarajithan G.
+
+Create Date: 03/11/2020
+Design Name: ALWAYS_VALID_CYCLIC_BRAM
+Tool Versions: Vivado 2018.2
+Description: * Wrapper around BRAM to make it behave like always ready
+                (hide its latency)
+             * Valid goes high only after filling the entire RAM
+             * Problems
+                - Has a buffer of depth LATENCY+1
+                    If BRAM is too wide (96 bytes), will take too many ffs here.
+                - Can ommit the output fabric FF of BRAM, to have 2 latency
+                    BRAM's output comes through a mux into buffer
+                - m_data is not registered (Buf via mux)
+
+Revision:
+Revision 0.01 - File Created
+Additional Comments: 
+
+//////////////////////////////////////////////////////////////////////////////////*/
+
 module always_valid_cyclic_bram #(
   DEPTH   = 8 ,
   WIDTH   = 64,
@@ -25,16 +48,17 @@ module always_valid_cyclic_bram #(
   input  logic [ADDR_WIDTH-1:0] addr_max_1;
 
   
-  logic [WIDTH-1:0] data_r;
   logic m_valid_ready;
+  assign m_valid_ready = m_valid && m_ready;
 
   /*
-    REGISTERS
+    STATES
+      - IDLE : Fill buffer with s_data
+      - WORK : Fill buffer with bram_dout
   */
 
   localparam IDLE = 0;
   localparam WORK = 1;
-
   logic state, state_next;
   register #(
     .WORD_WIDTH   (1), 
@@ -48,39 +72,20 @@ module always_valid_cyclic_bram #(
   );
   always_comb begin
     if (state == IDLE) begin
-      if (w_ptr == BUFFER_DEPTH-1) state_next = WORK;
-      else                         state_next = IDLE;
+      if (w_ptr == BUFFER_DEPTH-1 && s_valid_ready) begin
+            state_next = WORK;
+      end
+      else  state_next = IDLE;
     end
-    else                           state_next = WORK;
+    else    state_next = WORK;
   end
 
-  logic [WIDTH-1:0] buffer_data       [BUFFER_DEPTH];
-  logic [WIDTH-1:0] buffer_data_next  [BUFFER_DEPTH];
-  generate
-    for(genvar i=0; i < BUFFER_DEPTH; i++) begin: buf_data
-      register #(
-        .WORD_WIDTH   (WIDTH), 
-        .RESET_VALUE  (0)
-      ) BUFFER_DATA (
-        .clock        (clk   ),
-        .clock_enable (clken ),
-        .resetn       (resetn),
-        .data_in      (buffer_data_next[i]),
-        .data_out     (buffer_data     [i])
-      );
-    end
-  endgenerate
-  always_comb begin
-    buffer_data_next = buffer_data; // default
-    if (state == IDLE)begin
-      if (s_valid_ready)  buffer_data_next[w_ptr] = s_data;
-    end
-    else begin
-      if (bram_valid_out) buffer_data_next[w_ptr] = data_r;
-      else                buffer_data_next[w_ptr] = buffer_data_next[w_ptr];
-    end
-  end
+  /*
+    WRITE POINTER
 
+    - Counts cyclically from 0 to BUFFER_DEPTH-1
+        if "w_ptr_incr" (=s_valid_ready or bram_valid_out)
+  */
   logic w_ptr_incr;
   logic [BUFFER_DEPTH-1:0] w_ptr, w_ptr_next;
   register #(
@@ -106,6 +111,47 @@ module always_valid_cyclic_bram #(
     end
   end
 
+  /*
+    BUFFER
+
+    - buffer[w_ptr] <= s_data OR bram_data
+    - m_data = buffer[r_ptr]
+  */
+
+  logic [WIDTH-1:0] buffer_data       [BUFFER_DEPTH];
+  logic [WIDTH-1:0] buffer_data_next  [BUFFER_DEPTH];
+  generate
+    for(genvar i=0; i < BUFFER_DEPTH; i++) begin: buf_data
+      register #(
+        .WORD_WIDTH   (WIDTH), 
+        .RESET_VALUE  (0)
+      ) BUFFER_DATA (
+        .clock        (clk   ),
+        .clock_enable (clken ),
+        .resetn       (resetn),
+        .data_in      (buffer_data_next[i]),
+        .data_out     (buffer_data     [i])
+      );
+    end
+  endgenerate
+  always_comb begin
+    m_data  = buffer_data[r_ptr];
+
+    buffer_data_next = buffer_data; // default
+    if (state == IDLE)begin
+      if (s_valid_ready)  buffer_data_next[w_ptr] = s_data;
+    end
+    else begin
+      if (bram_valid_out) buffer_data_next[w_ptr] = bram_r_data;
+      else                buffer_data_next[w_ptr] = buffer_data_next[w_ptr];
+    end
+  end
+
+  /*
+    BRAM_VALID_OUT
+    Simulates latency of BRAM for valid data
+  */
+
   logic bram_valid_out;
   n_delay #(
       .N          (LATENCY),
@@ -118,7 +164,11 @@ module always_valid_cyclic_bram #(
       .data_out   (bram_valid_out)
   );
 
-  logic [ADDR_WIDTH-1:0] addr_w, addr_w_next;
+  /*
+    BRAM_WRITE_ADDRESS
+  */
+
+  logic [ADDR_WIDTH-1:0] addr_w_prev, addr_w;
   register #(
     .WORD_WIDTH   (ADDR_WIDTH), 
     .RESET_VALUE  (-1)
@@ -126,19 +176,22 @@ module always_valid_cyclic_bram #(
     .clock        (clk        ),
     .clock_enable (clken      ),
     .resetn       (resetn     ),
-    .data_in      (addr_w_next),
-    .data_out     (addr_w     )
+    .data_in      (addr_w),
+    .data_out     (addr_w_prev     )
   );
   always_comb begin
     if (s_valid_ready) begin
-      if (addr_w == addr_max_1) addr_w_next = 0;
-      else                      addr_w_next = addr_w + 1;
+      if (addr_w_prev == addr_max_1) addr_w = 0;
+      else                           addr_w = addr_w_prev + 1;
     end
     else begin
-      addr_w_next = addr_w;
+      addr_w = addr_w_prev;
     end
   end
 
+  /*
+    BRAM READ ADDRESS
+  */
 
   logic [ADDR_WIDTH-1:0] addr_r, addr_r_next;
   logic [BUFFER_DEPTH-1:0] r_ptr, r_ptr_next;
@@ -176,31 +229,37 @@ module always_valid_cyclic_bram #(
     end
   end
 
+  /*
+    M_VALID
+
+    * Goes high after filling the entire BRAM
+    * Writing to BRAM happens via AXIS. It may stop, start and take long time to fil
+      completely.
+    * Hence, cannot guarantee all data in BRAM are valid during writing.
+    * Can check the write address and pull valid down -- but that won't be always valid
+  */
   register #(
     .WORD_WIDTH   (1), 
     .RESET_VALUE  (0)
   ) MVALID (
     .clock        (clk    ),
-    .clock_enable (clken && (addr_w_next==addr_max_1) && s_valid_ready),
+    .clock_enable (clken && (addr_w==addr_max_1) && s_valid_ready),
     .resetn       (resetn ),
     .data_in      (1),
     .data_out     (m_valid)
   );
-  always_comb begin
-    m_data      = buffer_data[r_ptr];
-    m_valid_ready = m_valid && m_ready;
-  end
 
+  logic [WIDTH-1:0] bram_r_data;
   sdpram sdpram (
     .clka (clk          ),
     .ena  (clken        ),
     .wea  (s_valid_ready),
-    .addra(addr_w_next  ),
+    .addra(addr_w       ),
     .dina (s_data       ),
     .clkb (clk          ),
     .enb  (clken        ),
     .addrb(addr_r       ),
-    .doutb(data_r       )
+    .doutb(bram_r_data       )
   );
   
 endmodule
