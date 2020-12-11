e77de3c195d556b866cff11fdeb2bdf7a584393b
aba <abarajithan07@gmail.com>
2020-12-12 00:11:07 +0530

    Update and Test always_valid_bram with different input output width

--- src/always_valid_cyclic_bram.sv
+++ src/always_valid_cyclic_bram.sv
@@ -22,8 +22,9 @@ Additional Comments:
 //////////////////////////////////////////////////////////////////////////////////*/
 
 module always_valid_cyclic_bram #(
-  DEPTH   = 8 ,
-  WIDTH   = 64,
+  W_DEPTH = 8,
+  W_WIDTH = 8,
+  R_WIDTH = 8,
   LATENCY = 3
 )(
   clk  ,
@@ -34,104 +35,112 @@ module always_valid_cyclic_bram #(
   m_data,
   m_valid,
   m_ready,
-  addr_max_1
+  r_addr_max_1,
+  w_addr_max_1
 );
+  localparam R_DEPTH = W_DEPTH * W_WIDTH / R_WIDTH;
+  localparam W_ADDR_WIDTH = $clog2(W_DEPTH);
+  localparam R_ADDR_WIDTH = $clog2(R_DEPTH);
+
   localparam BUFFER_DEPTH = LATENCY + 1;
-  localparam ADDR_WIDTH = $clog2(DEPTH);
-  localparam BUFFER_ADDR_WIDTH = $clog2(LATENCY+1);
+  localparam PTR_WIDTH    = $clog2(LATENCY+1);
 
   input  logic clk, clken, resetn;
   input  logic s_valid_ready, m_ready;
   output logic m_valid;
-  input  logic [WIDTH-1:0] s_data;
-  output logic [WIDTH-1:0] m_data;
-  input  logic [ADDR_WIDTH-1:0] addr_max_1;
+  input  logic [W_WIDTH-1:0] s_data;
+  output logic [R_WIDTH-1:0] m_data;
+  input  logic [R_ADDR_WIDTH-1:0] r_addr_max_1;
+  input  logic [W_ADDR_WIDTH-1:0] w_addr_max_1;
 
   
   logic m_valid_ready;
   assign m_valid_ready = m_valid && m_ready;
+  logic bram_r_en;
 
   /*
-    STATES
-      - IDLE : Fill buffer with s_data
-      - WORK : Fill buffer with bram_dout
+    STATE MACHINE
   */
-
-  localparam IDLE = 0;
-  localparam WORK = 1;
-  logic state;
-  logic state_trigger;
-
-  assign state_trigger = (w_ptr == BUFFER_DEPTH-1 && s_valid_ready);
+  localparam WRITE_S  = 0;
+  localparam FILL_1_S = 1;
+  localparam FILL_2_S = 2;
+  localparam READ_S   = 3;  
+  logic [1:0] state, state_next;
 
   register #(
-    .WORD_WIDTH   (1), 
-    .RESET_VALUE  (IDLE)
+    .WORD_WIDTH   (2), 
+    .RESET_VALUE  (WRITE_S),
+    .LOCAL        (1)
   ) STATE (
-    .clock        (clk   ),
-    .clock_enable (clken && state_trigger),
+    .clock        (clk),
+    .clock_enable (clken),
     .resetn       (resetn),
-    .data_in      (WORK  ),
-    .data_out     (state )
+    .data_in      (state_next),
+    .data_out     (state)
   );
 
+  always_comb begin
+    state_next = state; // else
+    unique case (state)
+      WRITE_S : if(addr_w == w_addr_max_1   && s_valid_ready) state_next = FILL_1_S;
+      FILL_1_S: if(bram_valid_out)                            state_next = FILL_2_S;
+      FILL_2_S: if(w_ptr  == BUFFER_DEPTH-1)                  state_next = READ_S  ;
+    endcase
+  end
+
+  always_comb begin
+    bram_r_en = 0;
+    m_valid = 0;
+    unique case (state)
+      FILL_1_S: begin
+                  bram_r_en = 1;
+                end
+      FILL_2_S: begin
+                  bram_r_en = 1;
+                  m_valid   = 1;
+                end
+      READ_S  : begin
+                  bram_r_en = m_ready;
+                  m_valid = 1;
+                end
+    endcase
+  end
+
   /*
-    WRITE POINTER
-
-    - Counts cyclically from 0 to BUFFER_DEPTH-1
-        if "w_ptr_incr" (=s_valid_ready or bram_valid_out)
+    BRAM_WRITE_ADDRESS
   */
-  logic w_ptr_incr;
-  logic [BUFFER_DEPTH-1:0] w_ptr, w_ptr_next;
 
-  assign w_ptr_incr = (state == IDLE) ? s_valid_ready : bram_valid_out;
-  assign w_ptr_next = (w_ptr == BUFFER_DEPTH-1) ? 0   : w_ptr + 1;
+  logic [W_ADDR_WIDTH-1:0] addr_w_prev, addr_w;
+  assign addr_w = (addr_w_prev == w_addr_max_1) ? 0 : addr_w_prev + 1;
 
   register #(
-    .WORD_WIDTH   (BUFFER_DEPTH), 
-    .RESET_VALUE  (0)
-  ) W_PTR (
-    .clock        (clk   ),
-    .clock_enable (clken && w_ptr_incr),
-    .resetn       (resetn),
-    .data_in      (w_ptr_next),
-    .data_out     (w_ptr     )
+    .WORD_WIDTH   (W_ADDR_WIDTH), 
+    .RESET_VALUE  (-1)
+  ) ADDR_W (
+    .clock        (clk        ),
+    .clock_enable (clken && s_valid_ready),
+    .resetn       (resetn     ),
+    .data_in      (addr_w),
+    .data_out     (addr_w_prev)
   );
 
   /*
-    BUFFER
-
-    - buffer[w_ptr] <= s_data OR bram_data
-    - m_data = buffer[r_ptr]
+    BRAM READ ADDRESS
   */
 
-  logic [WIDTH-1:0] buffer_data       [BUFFER_DEPTH];
-  logic [WIDTH-1:0] buffer_data_next  [BUFFER_DEPTH];
+  logic [R_ADDR_WIDTH-1:0] addr_r, addr_r_next;
+  assign addr_r_next = (addr_r == r_addr_max_1) ? 0 : addr_r + 1;
 
-  logic buff_w_trigger;
-  assign buff_w_trigger = (state == IDLE) ? s_valid_ready : bram_valid_out;
-  assign m_data  = buffer_data[r_ptr];
-
-  generate
-    for(genvar i=0; i < BUFFER_DEPTH; i++) begin: buf_data
-      register #(
-        .WORD_WIDTH   (WIDTH), 
-        .RESET_VALUE  (0)
-      ) BUFFER_DATA (
-        .clock        (clk   ),
-        .clock_enable (clken && buff_w_trigger),
-        .resetn       (resetn),
-        .data_in      (buffer_data_next[i]),
-        .data_out     (buffer_data     [i])
-      );
-    end
-  endgenerate
-  always_comb begin
-    buffer_data_next = buffer_data; // default
-    if      (s_valid_ready)  buffer_data_next[w_ptr] = s_data;
-    else if (bram_valid_out) buffer_data_next[w_ptr] = bram_r_data;
-    else                     buffer_data_next[w_ptr] = buffer_data[w_ptr];
-  end
+  register #(
+    .WORD_WIDTH   (R_ADDR_WIDTH), 
+    .RESET_VALUE  (0)
+  ) ADD_R (
+    .clock        (clk   ),
+    .clock_enable (clken && bram_r_en),
+    .resetn       (resetn),
+    .data_in      (addr_r_next),
+    .data_out     (addr_r     )
+  );
 
   /*
     BRAM_VALID_OUT
@@ -146,52 +155,80 @@ module always_valid_cyclic_bram #(
       .clk        (clk           ),
       .resetn     (resetn        ),
       .clken      (clken         ),
-      .data_in    (m_valid_ready ),
+      .data_in    (bram_r_en     ),
       .data_out   (bram_valid_out)
   );
 
-  /*
-    BRAM_WRITE_ADDRESS
-  */
-
-  logic [ADDR_WIDTH-1:0] addr_w_prev, addr_w;
-  assign addr_w = (addr_w_prev == addr_max_1) ? 0 : addr_w_prev + 1;
-
-  register #(
-    .WORD_WIDTH   (ADDR_WIDTH), 
-    .RESET_VALUE  (-1)
-  ) ADDR_W (
-    .clock        (clk        ),
-    .clock_enable (clken && s_valid_ready),
-    .resetn       (resetn     ),
-    .data_in      (addr_w),
-    .data_out     (addr_w_prev)
+  logic [R_WIDTH-1:0] bram_r_data;
+  sdpram sdpram (
+    .clka (clk               ),
+    .ena  (clken             ),
+    .wea  (s_valid_ready     ),
+    .addra(addr_w            ),
+    .dina (s_data            ),
+    .clkb (clk               ),
+    .enb  (clken             ),
+    .addrb(addr_r            ),
+    .doutb(bram_r_data       )
   );
 
   /*
-    BRAM READ ADDRESS
-  */
+    WRITE POINTER
 
-  logic [ADDR_WIDTH-1:0] addr_r, addr_r_next;
-  assign addr_r_next = (addr_r == addr_max_1) ? 0 : addr_r + 1;
+    - Counts cyclically from 0 to BUFFER_DEPTH-1
+        if "w_ptr_incr" (=s_valid_ready or bram_valid_out)
+  */
+  logic [PTR_WIDTH-1:0] w_ptr, w_ptr_next;
+
+  assign w_ptr_next = (w_ptr == BUFFER_DEPTH-1) ? 0  : w_ptr + 1;
 
   register #(
-    .WORD_WIDTH   (ADDR_WIDTH), 
-    .RESET_VALUE  (BUFFER_DEPTH)
-  ) ADD_R (
-    .clock        (clk   ),
-    .clock_enable (clken && m_valid_ready),
+    .WORD_WIDTH   (PTR_WIDTH), 
+    .RESET_VALUE  (0),
+    .LOCAL        (1)
+  ) W_PTR (
+    .clock        (clk),
+    .clock_enable (clken && bram_valid_out),
     .resetn       (resetn),
-    .data_in      (addr_r_next),
-    .data_out     (addr_r     )
+    .data_in      (w_ptr_next),
+    .data_out     (w_ptr     )
   );
 
-  logic [BUFFER_DEPTH-1:0] r_ptr, r_ptr_next;
+  /*
+    BUFFER
+
+    - buffer[w_ptr] <= bram_data
+    - m_data = buffer[r_ptr]
+  */
+
+  logic [R_WIDTH-1:0] buffer_data       [BUFFER_DEPTH];
+  logic               buffer_en         [BUFFER_DEPTH];
+
+  generate
+    for(genvar i=0; i < BUFFER_DEPTH; i++) begin: buf_data
+
+      assign buffer_en[i] = (w_ptr == i) && bram_valid_out;
+
+      register #(
+        .WORD_WIDTH   (R_WIDTH), 
+        .RESET_VALUE  (0)
+      ) BUFFER_DATA (
+        .clock        (clk   ),
+        .clock_enable (clken && buffer_en[i]),
+        .resetn       (resetn),
+        .data_in      (bram_r_data),
+        .data_out     (buffer_data [i])
+      );
+    end
+  endgenerate
+
+  logic [PTR_WIDTH-1:0] r_ptr, r_ptr_next;
   assign r_ptr_next = (r_ptr == BUFFER_DEPTH-1) ? 0 : r_ptr  + 1;
 
   register #(
-    .WORD_WIDTH   (BUFFER_DEPTH), 
-    .RESET_VALUE  (0)
+    .WORD_WIDTH   (PTR_WIDTH), 
+    .RESET_VALUE  (0),
+    .LOCAL        (1)
   ) R_PTR (
     .clock        (clk   ),
     .clock_enable (clken && m_valid_ready),
@@ -200,37 +237,6 @@ module always_valid_cyclic_bram #(
     .data_out     (r_ptr     )
   );
 
-  /*
-    M_VALID
-
-    * Goes high after filling the entire BRAM
-    * Writing to BRAM happens via AXIS. It may stop, start and take long time to fil
-      completely.
-    * Hence, cannot guarantee all data in BRAM are valid during writing.
-    * Can check the write address and pull valid down -- but that won't be always valid
-  */
-  register #(
-    .WORD_WIDTH   (1), 
-    .RESET_VALUE  (0)
-  ) MVALID (
-    .clock        (clk    ),
-    .clock_enable (clken && (addr_w==addr_max_1) && s_valid_ready),
-    .resetn       (resetn ),
-    .data_in      (1),
-    .data_out     (m_valid)
-  );
-
-  logic [WIDTH-1:0] bram_r_data;
-  sdpram sdpram (
-    .clka (clk          ),
-    .ena  (clken        ),
-    .wea  (s_valid_ready),
-    .addra(addr_w       ),
-    .dina (s_data       ),
-    .clkb (clk          ),
-    .enb  (clken        ),
-    .addrb(addr_r       ),
-    .doutb(bram_r_data  )
-  );
+  assign m_data  = buffer_data[r_ptr];
   
 endmodule
