44aaef720408e2b7a2152e6a3460d212898ea240
aba <abarajithan07@gmail.com>
2021-01-24 21:43:19 +0530

    Test Input+Conv (MCGU=1214) with Layer 1 (3x3, maxpool) for one itr. Zero error

--- projects/build_integration.tcl
+++ projects/build_integration.tcl
@@ -9,7 +9,7 @@ set WORD_WIDTH_LRELU_1   32
 set WORD_WIDTH_LRELU_2   16
 set WORD_WIDTH_LRELU_OUT 8
 
-set UNITS   2
+set UNITS   4
 set GROUPS  1
 set COPIES  2
 set MEMBERS 1
@@ -240,23 +240,23 @@ lappend IP_NAMES $IP_NAME
 create_ip -name floating_point -vendor xilinx.com -library ip -version 7.1 -module_name $IP_NAME
 set_property -dict [list  CONFIG.Operation_Type {Float_to_fixed} CONFIG.A_Precision_Type {Half} CONFIG.C_Result_Exponent_Width {8} CONFIG.C_Result_Fraction_Width {0} CONFIG.Flow_Control {NonBlocking} CONFIG.Has_ACLKEN {true} CONFIG.Has_ARESETn {false} CONFIG.C_A_Exponent_Width {5} CONFIG.C_A_Fraction_Width {11} CONFIG.Result_Precision_Type {Custom} CONFIG.C_Accum_Msb {32} CONFIG.C_Accum_Lsb {-24} CONFIG.C_Accum_Input_Msb {15} CONFIG.C_Mult_Usage {No_Usage} CONFIG.Has_RESULT_TREADY {false} CONFIG.C_Latency {5} CONFIG.C_Rate {1}] [get_ips $IP_NAME]
 
-set IP_NAME "bram_lrelu"
-lappend IP_NAMES $IP_NAME
-set R_WIDTH 16
-set R_DEPTH [expr "$MEMBERS * $KERNEL_W_MAX"]
-set W_WIDTH [expr "$MEMBERS * $WORD_WIDTH_IN"]
-set W_DEPTH [expr "$R_WIDTH * $R_DEPTH / $W_WIDTH"]
-create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name $IP_NAME
-set_property -dict [list  CONFIG.Memory_Type {Simple_Dual_Port_RAM} CONFIG.Assume_Synchronous_Clk {true} CONFIG.Write_Width_A $W_WIDTH CONFIG.Write_Depth_A $W_DEPTH CONFIG.Read_Width_A $W_WIDTH CONFIG.Operating_Mode_A {NO_CHANGE} CONFIG.Write_Width_B $R_WIDTH CONFIG.Read_Width_B $R_WIDTH CONFIG.Operating_Mode_B {READ_FIRST} CONFIG.Enable_B {Use_ENB_Pin} CONFIG.Register_PortA_Output_of_Memory_Primitives {false} CONFIG.Register_PortB_Output_of_Memory_Primitives {true} CONFIG.Port_B_Clock {100} CONFIG.Port_B_Enable_Rate {100}] [get_ips $IP_NAME]
+# set IP_NAME "bram_lrelu"
+# lappend IP_NAMES $IP_NAME
+# set R_WIDTH 16
+# set R_DEPTH [expr "$MEMBERS * $KERNEL_W_MAX"]
+# set W_WIDTH [expr "$MEMBERS * $WORD_WIDTH_IN"]
+# set W_DEPTH [expr "$R_WIDTH * $R_DEPTH / $W_WIDTH"]
+# create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name $IP_NAME
+# set_property -dict [list  CONFIG.Memory_Type {Simple_Dual_Port_RAM} CONFIG.Assume_Synchronous_Clk {true} CONFIG.Write_Width_A $W_WIDTH CONFIG.Write_Depth_A $W_DEPTH CONFIG.Read_Width_A $W_WIDTH CONFIG.Operating_Mode_A {NO_CHANGE} CONFIG.Write_Width_B $R_WIDTH CONFIG.Read_Width_B $R_WIDTH CONFIG.Operating_Mode_B {READ_FIRST} CONFIG.Enable_B {Use_ENB_Pin} CONFIG.Register_PortA_Output_of_Memory_Primitives {false} CONFIG.Register_PortB_Output_of_Memory_Primitives {true} CONFIG.Port_B_Clock {100} CONFIG.Port_B_Enable_Rate {100}] [get_ips $IP_NAME]
 
-set IP_NAME "bram_lrelu_edge"
-lappend IP_NAMES $IP_NAME
-set R_WIDTH 16
-set R_DEPTH [expr "$MEMBERS"]
-set W_WIDTH [expr "$MEMBERS * $WORD_WIDTH_IN"]
-set W_DEPTH [expr "$R_WIDTH * $R_DEPTH / $W_WIDTH"]
-create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name $IP_NAME
-set_property -dict [list  CONFIG.Memory_Type {Simple_Dual_Port_RAM} CONFIG.Assume_Synchronous_Clk {true} CONFIG.Write_Width_A $W_WIDTH CONFIG.Write_Depth_A $W_DEPTH CONFIG.Read_Width_A $W_WIDTH CONFIG.Operating_Mode_A {NO_CHANGE} CONFIG.Write_Width_B $R_WIDTH CONFIG.Read_Width_B $R_WIDTH CONFIG.Operating_Mode_B {READ_FIRST} CONFIG.Enable_B {Use_ENB_Pin} CONFIG.Register_PortA_Output_of_Memory_Primitives {false} CONFIG.Register_PortB_Output_of_Memory_Primitives {true} CONFIG.Port_B_Clock {100} CONFIG.Port_B_Enable_Rate {100}] [get_ips $IP_NAME]
+# set IP_NAME "bram_lrelu_edge"
+# lappend IP_NAMES $IP_NAME
+# set R_WIDTH 16
+# set R_DEPTH [expr "$MEMBERS"]
+# set W_WIDTH [expr "$MEMBERS * $WORD_WIDTH_IN"]
+# set W_DEPTH [expr "$R_WIDTH * $R_DEPTH / $W_WIDTH"]
+# create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name $IP_NAME
+# set_property -dict [list  CONFIG.Memory_Type {Simple_Dual_Port_RAM} CONFIG.Assume_Synchronous_Clk {true} CONFIG.Write_Width_A $W_WIDTH CONFIG.Write_Depth_A $W_DEPTH CONFIG.Read_Width_A $W_WIDTH CONFIG.Operating_Mode_A {NO_CHANGE} CONFIG.Write_Width_B $R_WIDTH CONFIG.Read_Width_B $R_WIDTH CONFIG.Operating_Mode_B {READ_FIRST} CONFIG.Enable_B {Use_ENB_Pin} CONFIG.Register_PortA_Output_of_Memory_Primitives {false} CONFIG.Register_PortB_Output_of_Memory_Primitives {true} CONFIG.Port_B_Clock {100} CONFIG.Port_B_Enable_Rate {100}] [get_ips $IP_NAME]
 
 #*********** MAXPOOL **********#
 
