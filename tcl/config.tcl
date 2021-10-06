0225a0077723770cecc4dd7fc26c0779249cc8e4
abarajithan11 <abarajithan07@gmail.com>
2021-10-06 11:50:03 +0530

    Synth: 800MHz

--- tcl/config.tcl
+++ tcl/config.tcl
@@ -42,7 +42,7 @@ if {$XILINX} {
   set LATENCY_MULTIPLIER    0
   set LATENCY_ACCUMULATOR   1
 
-  set FREQ_HIGH        600
+  set FREQ_HIGH        800
   set WORD_WIDTH       16
   set WORD_WIDTH_ACC   24
   set S_WEIGHTS_WIDTH_LF 64
