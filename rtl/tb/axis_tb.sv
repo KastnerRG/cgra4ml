74373976197836f66c9501cdb38b661a868a71af
Aba <abarajithan-7@gmail.com>
2022-12-06 23:53:41 -0800

    Rewrite axis_tb to support iverilog

--- rtl/tb/axis_tb.sv
+++ rtl/tb/axis_tb.sv
@@ -1,24 +1,28 @@
+// This was first written using classes: https://github.com/abarajithan11/dnn-engine/blob/1b7742d24c1ece4e47340f7402e51b54c6d087a3/rtl/tb/axis_tb.sv
+// But iVerilog does not support {class, ref, break, rand}
+
 `timescale 1ns/1ps
 
-class AXIS_Sink #(WORD_WIDTH=8, BUS_WIDTH=8, READY_PROB=100);
+module AXIS_Sink #(
+  parameter  WORD_WIDTH=8, BUS_WIDTH=8, PROB_READY=20, FILE_PATH="",
+  parameter  WORDS_PER_BEAT = BUS_WIDTH/WORD_WIDTH
+)(
+    input  logic aclk, aresetn,
+    output logic m_ready,
+    input  logic m_valid, m_last,
+    input  logic [WORDS_PER_BEAT-1:0][WORD_WIDTH-1:0] m_data, 
+    input  logic [WORDS_PER_BEAT-1:0] m_keep
+);
 
-  localparam WORDS_PER_BEAT = BUS_WIDTH/WORD_WIDTH;
-  string file_path;
-  int status, file, i_words = 0;
-  rand bit random;
-  constraint c { random dist { 0 := (100-READY_PROB), 1 := (READY_PROB)}; };
+  int i_words = 0, file;
 
-  function new(string file_path);
-    this.file_path = file_path;
-    file = $fopen(file_path, "w"); // Open and close in "w" to initialize empty file
+  task axis_pull;
+    // clear the file
+    file = $fopen(FILE_PATH, "w");
     $fclose(file);
-  endfunction
+    file = $fopen(FILE_PATH, "a");
 
-  task axis_pull(
-    ref logic aclk, aresetn, m_ready, m_valid, m_last,
-    ref logic [WORDS_PER_BEAT-1:0][WORD_WIDTH-1:0] m_data, 
-    ref logic [WORDS_PER_BEAT-1:0] m_keep
-  );
+    // start at reset
     m_ready = 0;
     wait(aresetn);
     
@@ -27,119 +31,113 @@ class AXIS_Sink #(WORD_WIDTH=8, BUS_WIDTH=8, READY_PROB=100);
       @(posedge aclk)
       if (m_ready && m_valid) begin  // read at posedge
 
-        file = $fopen(file_path, "a"); // open and close file in "a" on each beat
+        file = $fopen(FILE_PATH, "a"); // open and close file in "a" on each beat
         if (file==0) 
-          $fatal(1, "File '%s' does not exist\n", file_path);
+          $fatal(1, "File '%s' does not exist\n", FILE_PATH);
 
         for (int i=0; i < WORDS_PER_BEAT; i=i+1)
           if (m_keep[i]) begin
-            $fdisplay(file, "%d", signed'(m_data[i]));
+            $fdisplay(file, "%d", m_data[i]);
             i_words  += 1;
           end
         $fclose(file);
       end
 
       #1 // delay before writing
-      this.randomize(); // random m_ready at every cycle
-      m_ready = random;
+      m_ready = $urandom_range(0,99) < PROB_READY;
 
     end
   endtask
-endclass
+endmodule
 
 
+module AXIS_Source #(
+  parameter WORD_WIDTH=8, BUS_WIDTH=8, PROB_VALID=20, FILE_PATH="",
+  parameter WORDS_PER_BEAT = BUS_WIDTH/WORD_WIDTH
+)(
+    input  logic aclk, aresetn, s_ready, 
+    output logic s_valid, s_last,
+    output logic [WORDS_PER_BEAT-1:0][WORD_WIDTH-1:0] s_data,
+    output logic [WORDS_PER_BEAT-1:0] s_keep
+); 
+  int status, i_words=0, file, val;
+  logic prev_handshake=0, prev_slast=0;
 
-class AXIS_Source #(WORD_WIDTH=8, BUS_WIDTH=8, VALID_PROB=100);
-
-  localparam WORDS_PER_BEAT = BUS_WIDTH/WORD_WIDTH;
-  string file_path;
-  int status, i_words=0, file=0;
-  logic prev_handshake, prev_slast;
-
-  rand bit random;
-  constraint c { random dist { 0 := (100-VALID_PROB), 1 := (VALID_PROB)}; };
-
-  function new(string file_path);
-    this.file_path = file_path;
-    this.file = $fopen(this.file_path, "r");
-    if (this.file == 0) $fatal(1, "File '%s' does not exist\n", file_path);
-  endfunction
-
-
-  task axis_push(
-    ref logic aclk, aresetn, s_ready, s_valid, s_last,
-    ref logic [WORDS_PER_BEAT-1:0][WORD_WIDTH-1:0] s_data,
-    ref logic [WORDS_PER_BEAT-1:0] s_keep
-  );
+  task axis_push();
     {s_valid, s_data, s_last, s_keep} = '0;
 
+    file = $fopen(FILE_PATH, "r");
+    if (file == 0) 
+      $fatal(1, "File '%s' does not exist\n", FILE_PATH);
+
     wait(aresetn & s_ready); // wait for slave to begin
-
-    while (1) begin          // loop goes from (aresetn & s_ready) to s_last
-
-      @(posedge aclk);
-      prev_handshake = s_valid && s_ready; // read at posedge
-      prev_slast     = s_valid && s_ready && s_last;
-      
-      #1 // Delay before writing s_valid, s_data, s_keep
-      
-      if (prev_slast) begin
-        // Reset & close packet after done
-        {s_valid, s_data, s_keep, s_last} = '0;
-        $display("Closing file '%s' at i_words=%d \n", file_path, i_words);
-        $fclose(file);
-        return;
-      end
-
+    
+    // iverilog doesnt support break. so the loop is rolled to have break at top
+    while (~prev_slast) begin    // loop goes from (aresetn & s_ready) to s_last
       if (prev_handshake) begin  // change data
         for (int i=0; i < WORDS_PER_BEAT; i++) begin
           if($feof(file)) begin
-            $display(1, "EOF found at i_words=%d, path=%s \n", i_words, file_path); // If files ends in the middle of beat, fill rest with zeros
+            $display(1, "EOF found at i_words=%d, path=%s \n", i_words, FILE_PATH); // If files ends in the middle of beat, fill rest with zeros
             s_data[i] = 0;
             s_keep[i] = 0;
           end
           else begin
-            status = $fscanf(file,"%d\n", s_data[i]);
+            $fscanf(file,"%d\n", val);
+            s_data[i] = val;
             s_keep[i] = 1;
             i_words  += 1;
           end
           s_last = $feof(file); // need to check one extra time to catch eof
         end
       end
+      s_valid = $urandom_range(0,99) < PROB_VALID;      // randomize s_valid
 
-      this.randomize();
-      s_valid = random;      // randomize s_valid
+      // -------------- LOOP BEGINS HERE -----------
+      @(posedge aclk);
+      prev_handshake = s_valid && s_ready; // read at posedge
+      prev_slast     = s_valid && s_ready && s_last;
+      
+      #1; // Delay before writing s_valid, s_data, s_keep
     end
+
+    // Reset & close packet after done
+    {s_valid, s_data, s_keep, s_last} = '0;
+    $display("Closing file '%s' at i_words=%d \n", FILE_PATH, i_words);
+    $fclose(file);
+
   endtask
-endclass
+endmodule
 
 
 module axis_tb;
 
-  localparam WORD_W = 16, BUS_W=64, VALID_PROB=20, READY_PROB=20, NUM_DATA = ((BUS_W/WORD_W)*101)/2;
+  localparam WORD_W = 16, BUS_W=64, PROB_VALID=20, PROB_READY=20, NUM_DATA=((BUS_W/WORD_W)*101)/2;
+
   logic aclk=0, aresetn, i_ready, i_valid, i_last;
   logic [BUS_W/WORD_W-1:0][WORD_W-1:0] i_data;
   logic [BUS_W/WORD_W-1:0]             i_keep;
   int file_in, file_out, status, value;
-  string file_path_in  = "D:/axis_test_in.txt";
-  string file_path_out = "D:/axis_test_out.txt";
+  localparam file_path_in  = "axis_test_in.txt";
+  localparam file_path_out = "axis_test_out.txt";
 
-
-  AXIS_Source #(WORD_W, BUS_W , VALID_PROB) source = new(file_path_in );
-  AXIS_Sink   #(WORD_W, BUS_W , READY_PROB) sink   = new(file_path_out);
-  
   initial forever #5 aclk = ~aclk;
-  initial sink  .axis_pull(aclk, aresetn, i_ready, i_valid, i_last, i_data, i_keep);
+
+  AXIS_Source #(.WORD_WIDTH(WORD_W), .BUS_WIDTH(BUS_W), .PROB_VALID(PROB_VALID), .FILE_PATH(file_path_in )) source (aclk, aresetn, i_ready, i_valid, i_last, i_data, i_keep);
+  AXIS_Sink   #(.WORD_WIDTH(WORD_W), .BUS_WIDTH(BUS_W), .PROB_READY(PROB_READY), .FILE_PATH(file_path_out)) sink   (aclk, aresetn, i_ready, i_valid, i_last, i_data, i_keep);
+  
+  initial sink.axis_pull;
   initial begin
     file_in = $fopen(file_path_in, "w");
     for (int i=0; i<NUM_DATA; i++)
       $fdisplay(file_in, "%d", i);
     $fclose(file_in);
-    source.axis_push(aclk, aresetn, i_ready, i_valid, i_last, i_data, i_keep);
+    source.axis_push;
   end
 
 
   initial begin
+    $dumpfile ("axis_tb.vcd"); $dumpvars;
+
     aresetn = 0;
     repeat(2) @(posedge aclk)
     aresetn = 1;
