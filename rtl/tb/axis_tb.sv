1b7742d24c1ece4e47340f7402e51b54c6d087a3
Aba <abarajithan-7@gmail.com>
2022-12-06 21:42:52 -0800

    Fix bugs in axis_tb. Tested

--- rtl/tb/axis_tb.sv
+++ rtl/tb/axis_tb.sv
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps
+
 class AXIS_Sink #(WORD_WIDTH=8, BUS_WIDTH=8, READY_PROB=100);
 
   localparam WORDS_PER_BEAT = BUS_WIDTH/WORD_WIDTH;
@@ -18,14 +20,12 @@ class AXIS_Sink #(WORD_WIDTH=8, BUS_WIDTH=8, READY_PROB=100);
     ref logic [WORDS_PER_BEAT-1:0] m_keep
   );
     m_ready = 0;
+    wait(aresetn);
     
     while (1) begin
-      @(posedge aclk) #1
-      if (~aresetn) continue;
-      this.randomize(); // random m_ready at every cycle
-      m_ready = random;
 
-      if (m_ready && m_valid) begin
+      @(posedge aclk)
+      if (m_ready && m_valid) begin  // read at posedge
 
         file = $fopen(file_path, "a"); // open and close file in "a" on each beat
         if (file==0) 
@@ -38,6 +38,11 @@ class AXIS_Sink #(WORD_WIDTH=8, BUS_WIDTH=8, READY_PROB=100);
           end
         $fclose(file);
       end
+
+      #1 // delay before writing
+      this.randomize(); // random m_ready at every cycle
+      m_ready = random;
+
     end
   endtask
 endclass
@@ -49,10 +54,10 @@ class AXIS_Source #(WORD_WIDTH=8, BUS_WIDTH=8, VALID_PROB=100);
   localparam WORDS_PER_BEAT = BUS_WIDTH/WORD_WIDTH;
   string file_path;
   int status, i_words=0, file=0;
+  logic prev_handshake, prev_slast;
 
   rand bit random;
   constraint c { random dist { 0 := (100-VALID_PROB), 1 := (VALID_PROB)}; };
-
 
   function new(string file_path);
     this.file_path = file_path;
@@ -71,14 +76,22 @@ class AXIS_Source #(WORD_WIDTH=8, BUS_WIDTH=8, VALID_PROB=100);
     wait(aresetn & s_ready); // wait for slave to begin
 
     while (1) begin          // loop goes from (aresetn & s_ready) to s_last
-      wait(s_ready);         // hold (data, valid, last) until slave accepts
-      if (s_last) break;     // If s_last has been accepted, packet done.
-      
-      #1                     // Delay before writing
-      this.randomize();
-      s_valid = random;      // randomize s_valid
 
-      if (s_valid) begin     // push data if s_valid
+      @(posedge aclk);
+      prev_handshake = s_valid && s_ready; // read at posedge
+      prev_slast     = s_valid && s_ready && s_last;
+      
+      #1 // Delay before writing s_valid, s_data, s_keep
+      
+      if (prev_slast) begin
+        // Reset & close packet after done
+        {s_valid, s_data, s_keep, s_last} = '0;
+        $display("Closing file '%s' at i_words=%d \n", file_path, i_words);
+        $fclose(file);
+        return;
+      end
+
+      if (prev_handshake) begin  // change data
         for (int i=0; i < WORDS_PER_BEAT; i++) begin
           if($feof(file)) begin
             $display(1, "EOF found at i_words=%d, path=%s \n", i_words, file_path); // If files ends in the middle of beat, fill rest with zeros
@@ -93,14 +106,54 @@ class AXIS_Source #(WORD_WIDTH=8, BUS_WIDTH=8, VALID_PROB=100);
           s_last = $feof(file); // need to check one extra time to catch eof
         end
       end
-      @(posedge aclk); // give a clock for data to be accepted
-      // no logic beyond here. s_valid is new. loop back, wait for s_ready (slave to accept)
+
+      this.randomize();
+      s_valid = random;      // randomize s_valid
     end
-
-    // Reset & close packet after done
-    {s_valid, s_data, s_keep, s_last} = '0;
-    $display("Closing file '%s' at i_words=%d \n", file_path, i_words);
-    $fclose(file);
-
   endtask
 endclass
+
+
+module axis_tb;
+
+  localparam WORD_W = 16, BUS_W=64, VALID_PROB=20, READY_PROB=20, NUM_DATA = ((BUS_W/WORD_W)*101)/2;
+  logic aclk=0, aresetn, i_ready, i_valid, i_last;
+  logic [BUS_W/WORD_W-1:0][WORD_W-1:0] i_data;
+  logic [BUS_W/WORD_W-1:0]             i_keep;
+  int file_in, file_out, status, value;
+  string file_path_in  = "D:/axis_test_in.txt";
+  string file_path_out = "D:/axis_test_out.txt";
+
+
+  AXIS_Source #(WORD_W, BUS_W , VALID_PROB) source = new(file_path_in );
+  AXIS_Sink   #(WORD_W, BUS_W , READY_PROB) sink   = new(file_path_out);
+  
+  initial forever #5 aclk = ~aclk;
+  initial sink  .axis_pull(aclk, aresetn, i_ready, i_valid, i_last, i_data, i_keep);
+  initial begin
+    file_in = $fopen(file_path_in, "w");
+    for (int i=0; i<NUM_DATA; i++)
+      $fdisplay(file_in, "%d", i);
+    $fclose(file_in);
+    source.axis_push(aclk, aresetn, i_ready, i_valid, i_last, i_data, i_keep);
+  end
+
+
+  initial begin
+    aresetn = 0;
+    repeat(2) @(posedge aclk)
+    aresetn = 1;
+
+    @(negedge i_last) 
+    @(posedge aclk)
+
+    file_out = $fopen(file_path_out, "r");
+    for (int i=0; i<NUM_DATA; i++) begin
+      status = $fscanf(file_out,"%d\n", value);
+      assert (value == i) else $error("Output does not match");
+    end
+
+    $finish();
+  end
+
+endmodule
