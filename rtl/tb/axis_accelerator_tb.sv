f7215529f535c398f6f61fe4fe7d501956f269e7
Aba <abarajithan-7@gmail.com>
2022-12-07 01:31:21 -0800

    Update acc_tb to match axis_tb

--- rtl/tb/axis_accelerator_tb.sv
+++ rtl/tb/axis_accelerator_tb.sv
@@ -58,23 +58,16 @@ module axis_accelerator_tb #(
 
               WORD_WIDTH_OUT             = WORD_WIDTH_ACC             ; 
 
-  logic aresetn;
-  logic hf_aresetn;
-  logic s_axis_pixels_tready;
-  logic s_axis_pixels_tvalid;
-  logic s_axis_pixels_tlast ;
+  logic aresetn, hf_aresetn;
+  logic s_axis_pixels_tready, s_axis_pixels_tvalid, s_axis_pixels_tlast;
   logic [S_PIXELS_WIDTH_LF/WORD_WIDTH -1:0][WORD_WIDTH-1:0] s_axis_pixels_tdata;
   logic [S_PIXELS_WIDTH_LF/WORD_WIDTH -1:0] s_axis_pixels_tkeep;
 
-  logic s_axis_weights_tready;
-  logic s_axis_weights_tvalid;
-  logic s_axis_weights_tlast ;
+  logic s_axis_weights_tready, s_axis_weights_tvalid, s_axis_weights_tlast;
   logic [S_WEIGHTS_WIDTH_LF/WORD_WIDTH-1:0][WORD_WIDTH-1:0] s_axis_weights_tdata;
   logic [S_WEIGHTS_WIDTH_LF/WORD_WIDTH-1:0] s_axis_weights_tkeep;
 
-  logic m_axis_tvalid;
-  logic m_axis_tready;
-  logic m_axis_tlast;
+  logic m_axis_tvalid, m_axis_tready, m_axis_tlast;
   logic [M_OUTPUT_WIDTH_LF/WORD_WIDTH_OUT-1:0][WORD_WIDTH_OUT-1:0] m_axis_tdata;
   logic [M_OUTPUT_WIDTH_LF/WORD_WIDTH_OUT-1:0] m_axis_tkeep;
 
@@ -84,14 +77,13 @@ module axis_accelerator_tb #(
 
   // SOURCEs & SINKS
 
-  AXIS_Source #(WORD_WIDTH    , S_PIXELS_WIDTH_LF , VALID_PROB) source_x = new({DIR_PATH, MODEL, "_conv_", IDX, "_x.txt"});
-  AXIS_Source #(WORD_WIDTH    , S_WEIGHTS_WIDTH_LF, VALID_PROB) source_w = new({DIR_PATH, MODEL, "_conv_", IDX, "_w.txt"});
-  AXIS_Sink   #(WORD_WIDTH_OUT, M_OUTPUT_WIDTH_LF , READY_PROB) sink_y   = new({DIR_PATH, MODEL, "_conv_", IDX, "_y_sim.txt"});
-  
-  initial source_x.axis_push(aclk, aresetn, s_axis_pixels_tready , s_axis_pixels_tvalid , s_axis_pixels_tlast , s_axis_pixels_tdata , s_axis_pixels_tkeep );
-  initial source_w.axis_push(aclk, aresetn, s_axis_weights_tready, s_axis_weights_tvalid, s_axis_weights_tlast, s_axis_weights_tdata, s_axis_weights_tkeep);
-  initial sink_y  .axis_pull(aclk, aresetn, m_axis_tready        , m_axis_tvalid        , m_axis_tlast        , m_axis_tdata        , m_axis_tkeep        );
+  AXIS_Source #(WORD_WIDTH    , S_PIXELS_WIDTH_LF , VALID_PROB, {DIR_PATH, MODEL, "_conv_", IDX, "_x.txt"    }) source_x (aclk, aresetn, s_axis_pixels_tready , s_axis_pixels_tvalid , s_axis_pixels_tlast , s_axis_pixels_tdata , s_axis_pixels_tkeep );
+  AXIS_Source #(WORD_WIDTH    , S_WEIGHTS_WIDTH_LF, VALID_PROB, {DIR_PATH, MODEL, "_conv_", IDX, "_w.txt"    }) source_k (aclk, aresetn, s_axis_weights_tready, s_axis_weights_tvalid, s_axis_weights_tlast, s_axis_weights_tdata, s_axis_weights_tkeep);
+  AXIS_Sink   #(WORD_WIDTH_OUT, M_OUTPUT_WIDTH_LF , READY_PROB, {DIR_PATH, MODEL, "_conv_", IDX, "_y_sim.txt"}) sink_y   (aclk, aresetn, m_axis_tready        , m_axis_tvalid        , m_axis_tlast        , m_axis_tdata        , m_axis_tkeep        );
 
+  initial source_x.axis_push;
+  initial source_k.axis_push;
+  initial sink_y  .axis_pull;
 
   // START SIM  
 
@@ -104,8 +96,10 @@ module axis_accelerator_tb #(
     wait(m_axis_tlast && m_axis_tvalid && m_axis_tready);
     @(posedge aclk) 
     $display("DONE. m_last accepted at sink_y.i_words=%d.", sink_y.i_words);
-    // $finish();
-    // bug - conv passes tlast for multiple clocks 
+
+    @(negedge m_axis_tlast)
+    @(posedge aclk)
+    $finish();
   end
 
 endmodule
