0cadc8b8dffe036fc2d09ef3b5409ad16cc57bb7
Aba <abarajithan-7@gmail.com>
2023-05-01 18:14:24 -0700

    Update weights rotator to replace counters, refactor

--- rtl/axis_weight_rotator.sv
+++ rtl/axis_weight_rotator.sv
@@ -1,28 +1,7 @@
-/*//////////////////////////////////////////////////////////////////////////////////
+/*
 Engineer: Abarajithan G.
-
-Create Date: 30/12/2020
-Design Name: AXIS Weight Rotator
-Tool Versions: Vivado 2018.2
-Description: 
-          - Contains two Always Valid Cyclic BRAMs and a DW converter
-          - s_data is directly from DMA. 32 bits.
-          - first beat contains ref values: {s_blocks_1, s_cols_1, s_cin_1, s_kh2 , s_kw2 }
-          - first beat bypasses DWC and loaded to ref registers
-          - Following data (lrelu_config: 21/13 m_beats + weights: k_h*cin m_beats) 
-              written into one BRAM
-          - When done_write, the BRAM is read and rotated (cols*blocks) times
-              - Total weights beats: k_h*cin*cols*blocks
-              - M_TLAST issued at last beat
-              - T_TUSER: is_config, is_1x1, kw, top, bot, cols_1_k2            
-
-Dependencies: 
-
-Revision:
-Revision 0.01 - File Created
-Additional Comments: 
-
-//////////////////////////////////////////////////////////////////////////////////*/
+Design Name: AXIS Weight Rotator    
+*/
 `timescale 1ns/1ps
 `include "../params/params.svh"
 
@@ -30,10 +9,7 @@ module axis_weight_rotator #(
   parameter 
     COLS                = `COLS                     ,
     WORD_WIDTH          = `WORD_WIDTH               , 
-    KH_MAX              = `KH_MAX                   ,   // odd number
     KW_MAX              = `KW_MAX                   ,   // odd number
-    SH_MAX              = `SH_MAX                   ,   // odd number
-    SW_MAX              = `SW_MAX                   ,   // odd number
     IM_CIN_MAX          = `IM_CIN_MAX               ,
     IM_COLS_MAX         = `IM_COLS_MAX              ,
     IM_ROWS_MAX         = `IM_ROWS_MAX              ,
@@ -44,9 +20,7 @@ module axis_weight_rotator #(
 
   localparam  
     BITS_KW2            = $clog2((KW_MAX+1)/2)     ,
-    BITS_KH2            = $clog2((KH_MAX+1)/2)     ,
-    BITS_SW             = $clog2(SW_MAX      )     ,
-    BITS_KH             = $clog2(KH_MAX      )     ,
+    BITS_KW             = $clog2(KW_MAX      )     ,
     BITS_IM_CIN         = $clog2(IM_CIN_MAX  )     ,
     BITS_IM_BLOCKS      = $clog2(IM_ROWS_MAX/`ROWS),
     BITS_IM_COLS        = $clog2(IM_COLS_MAX      ),
@@ -56,8 +30,8 @@ module axis_weight_rotator #(
     BRAM_DEPTH          = BRAM_WEIGHTS_DEPTH       ,
     BITS_ADDR           = $clog2(BRAM_WEIGHTS_DEPTH),
     BRAM_TYPE           = SRAM_TYPE == "XILINX" ? "XILINX_WEIGHTS" : SRAM_TYPE,
-    CONFIG_COUNT_MAX    = 1                        ,// lrelu_beats
-    BITS_CONFIG_COUNT   = $clog2(CONFIG_COUNT_MAX)
+    CONFIG_COUNT        = 1                        ,// lrelu_beats
+    BITS_CONFIG_COUNT   = $clog2(CONFIG_COUNT+1)
   )(
     
     input logic aclk,
@@ -66,8 +40,8 @@ module axis_weight_rotator #(
     output logic                                       s_axis_tready,
     input  logic                                       s_axis_tvalid,
     input  logic                                       s_axis_tlast ,
-    input  logic [S_WEIGHTS_WIDTH_LF    -1:0]          s_axis_tdata ,
-    input  logic [S_WEIGHTS_WIDTH_LF /WORD_WIDTH -1:0] s_axis_tkeep ,
+    input  logic [S_WEIGHTS_WIDTH_LF            -1:0]  s_axis_tdata ,
+    input  logic [S_WEIGHTS_WIDTH_LF/WORD_WIDTH -1:0]  s_axis_tkeep ,
 
     input  logic               m_axis_tready,
     output logic               m_axis_tvalid,
@@ -80,42 +54,21 @@ module axis_weight_rotator #(
   enum {R_IDLE_S, R_PASS_CONFIG_S, R_READ_S, R_SWITCH_S} state_read;
   enum {DW_PASS_S, DW_BLOCK_S} state_dw;
 
-  logic dw_m_ready, dw_m_valid, dw_m_last, dw_s_valid, dw_s_ready;
-  logic [M_WIDTH -1:0] dw_m_data_flat;
-
-  logic i_read, i_write;
-
-  logic [1:0] done_read_next, done_write_next, en_ref;
-  logic [1:0] done_read, done_write, bram_resetn, bram_wen, bram_w_full, bram_m_ready;
-  logic     bram_reg_resetn, bram_m_valid, bram_reg_m_valid;
-
-  logic [M_WIDTH-1:0]    bram_m_data  [2];
+  logic i_read, i_write, dw_m_ready, dw_m_valid, dw_m_last, dw_s_valid, dw_s_ready;
+  logic      [M_WIDTH-1:0] dw_m_data_flat;
+  logic [1:0][M_WIDTH-1:0] bram_m_data;
+  logic [1:0] done_read_next, done_write_next, en_ref, done_read, done_write, bram_resetn, bram_wen, bram_w_full, bram_m_ready;
+  logic       bram_reg_resetn, bram_m_valid, bram_reg_m_valid;
+  logic en_count_config, l_config, l_kw, l_cin, l_cols, l_blocks, f_kw, f_cin, f_cols, f_blocks, lc_config, lc_kw, lc_cin, lc_cols, lc_blocks;
+  struct packed {
+    logic [BITS_ADDR        -1:0] addr_max;
+    logic [BITS_IM_BLOCKS   -1:0] blocks_1;
+    logic [BITS_IM_COLS     -1:0] cols_1;
+    logic [BITS_IM_CIN      -1:0] cin_1;
+    logic [BITS_KW2         -1:0] kw2;
+  } s_config, count, ref_config [2];
   
-  logic [BITS_ADDR-1:0] s_addr_max, s_addr_min, r_addr_min [2], addr_max [2];
-  logic [BITS_CONFIG_COUNT-1:0] count_config, count_next_config;
-
-  logic [BITS_KW2         -1:0] s_kw2 , ref_kw2   [2];
-  logic [BITS_KH2         -1:0] s_kh2 , ref_kh2   [2];
-  logic [BITS_KH          -1:0] count_kh, count_next_kh;
-  logic [BITS_SW          -1:0] s_sw_1    , count_sw    , count_next_sw    , ref_1_sw     [2];
-  logic [BITS_IM_CIN      -1:0] s_cin_1   , count_cin   , count_next_cin   , ref_1_cin    [2];
-  logic [BITS_IM_COLS     -1:0] s_cols_1  , count_cols  , count_next_cols  , ref_1_cols   [2];
-  logic [BITS_IM_BLOCKS   -1:0] s_blocks_1, count_blocks, count_next_blocks, ref_1_blocks [2];
-  
-  logic en_count_kh, en_count_sw, en_count_cin, en_count_cols, en_count_blocks, en_count_config;
-  logic last_config, last_kh, last_sw, last_cin, last_cols, last_blocks;
-  logic last_next_config, last_next_kh, last_next_sw, last_next_cin, last_next_cols, last_next_blocks;
-  
-
-  // Total lut
-  localparam BEATS_TOTAL_MAX = 1; // lrelu_beats
-  localparam BITS_BEATS_TOTAL = $clog2(BEATS_TOTAL_MAX+1);
-  logic [BITS_BEATS_TOTAL-1:0] lut_lrelu_beats_1 [KW_MAX      /2:0];
-  generate
-    for (genvar KW2=0; KW2 <= KW_MAX      /2; KW2++)
-      assign lut_lrelu_beats_1[KW2] = 1 -1; // lrelu_beats
-  endgenerate
-
+  assign s_config = s_axis_tdata;
   wire s_handshake      = s_axis_tready && s_axis_tvalid;
   wire s_last_handshake = s_handshake   && s_axis_tlast;
 
@@ -162,18 +115,17 @@ module axis_weight_rotator #(
 
   //  STATE MACHINE: READ
   always_ff @(posedge aclk)
-    if (!aresetn)                                           state_read <= R_IDLE_S;
+    if (!aresetn)                                state_read <= R_IDLE_S;
     else unique case (state_read)
-      R_IDLE_S        : if (done_write [i_read])            state_read <= R_PASS_CONFIG_S;
-      R_PASS_CONFIG_S : if (en_count_config && last_config) state_read <= R_READ_S;
-      R_READ_S        : if (en_count_blocks && last_blocks) state_read <= R_SWITCH_S;
-      R_SWITCH_S      :                                     state_read <= R_IDLE_S;
+      R_IDLE_S        : if (done_write [i_read]) state_read <= R_PASS_CONFIG_S;
+      R_PASS_CONFIG_S : if (lc_config)           state_read <= R_READ_S;
+      R_READ_S        : if (lc_blocks)           state_read <= R_SWITCH_S;
+      R_SWITCH_S      :                          state_read <= R_IDLE_S;
     endcase 
 
   always_comb begin
 
     en_count_config   = 0;
-    count_next_config = lut_lrelu_beats_1[ref_kh2 [i_read]];
     m_axis_tvalid     = 0;
     bram_reg_resetn   = 1;
 
@@ -182,7 +134,6 @@ module axis_weight_rotator #(
                           en_count_config = 1;
                         end
       R_PASS_CONFIG_S : begin
-                          count_next_config  = count_config -1;
                           m_axis_tvalid      = bram_reg_m_valid;
                           en_count_config    = m_axis_tvalid && m_axis_tready;
                         end
@@ -224,11 +175,6 @@ module axis_weight_rotator #(
       s_axis_tready = dw_s_ready;
     end
   end
-
-  localparam SUM_BITS = BITS_ADDR + BITS_IM_BLOCKS + BITS_IM_COLS + BITS_IM_CIN + BITS_SW + BITS_KH2 + BITS_KW2;
-  assign {s_addr_max, s_blocks_1, s_cols_1, s_cin_1, s_sw_1, s_kh2 , s_kw2 } = s_axis_tdata[SUM_BITS-1:0]; // gives error if SUM_BITS > S_WEIGHTS_WIDTH
-  assign s_addr_min = lut_lrelu_beats_1[s_kh2] + 1;
-
 
   generate
     for (genvar i=0; i<2; i++) begin
@@ -282,9 +228,9 @@ module axis_weight_rotator #(
         .w_en         (bram_wen    [i]),
         .m_data       (bram_m_data [i]),
         .r_en         (bram_m_ready[i]),
-        .r_addr_min   (r_addr_min  [i]),
+        .r_addr_min   (CONFIG_COUNT   ),
         .w_last_in    (dw_m_last_handshake),
-        .r_addr_max   (addr_max    [i])
+        .r_addr_max   (ref_config  [i].addr_max)
       );
 
       /*
@@ -311,16 +257,7 @@ module axis_weight_rotator #(
 
       // Reference Registers
       always_ff @(posedge aclk)
-        if (en_ref[i]) begin
-          ref_kw2      [i] <= s_kw2     ;
-          ref_kh2      [i] <= s_kh2     ;
-          ref_1_sw     [i] <= s_sw_1    ;
-          ref_1_cin    [i] <= s_cin_1   ;
-          ref_1_cols   [i] <= s_cols_1  ;
-          ref_1_blocks [i] <= s_blocks_1;
-          r_addr_min   [i] <= s_addr_min;
-          addr_max     [i] <= s_addr_max;
-        end
+        if (en_ref[i]) ref_config [i] <= s_config;
     end
   endgenerate
 
@@ -354,182 +291,30 @@ module axis_weight_rotator #(
     .m_axis_tready(bram_m_ready[i_read])
   );
 
-  /*
-    COUNTER REGISTERS
+  // Counters
+  logic [BITS_IM_COLS -1:0] c_cols;
+  wire copy_config = (state_read == R_IDLE_S) && done_write [i_read];
+  wire en_kw       = m_axis_tvalid && m_axis_tready && state_read == R_READ_S;
 
-    - Nested counters: k_h -> cin -> cols -> blocks
-    - Down-counters
-      - count from (ref-1) to 0
-      - check 1, delay by 1 to get (count == 0)
-        - (count==1) check will fail if ref = 1, that is ref_1 == 0
-        - if ref_1==0, always last_next
-        - This will fail at ref=0, but who the fuck counts to zero?
+  counter #(.W(BITS_CONFIG_COUNT)) C_CONFIG    (.clk(aclk), .reset(copy_config), .en(en_count_config), .max_in(CONFIG_COUNT-1                ), .last_clk(lc_config), .last(l_config)                                  );
+  counter #(.W(BITS_KW          )) C_KW        (.clk(aclk), .reset(copy_config), .en(en_kw          ), .max_in(2*ref_config [i_read].kw2     ), .last_clk(lc_kw    ), .last(l_kw    ), .first(f_kw    )                );
+  counter #(.W(BITS_IM_CIN      )) C_IM_CIN    (.clk(aclk), .reset(copy_config), .en(lc_kw          ), .max_in(ref_config   [i_read].cin_1   ), .last_clk(lc_cin   ), .last(l_cin   ), .first(f_cin   )                );
+  counter #(.W(BITS_IM_COLS     )) C_IM_COLS   (.clk(aclk), .reset(copy_config), .en(lc_cin         ), .max_in(ref_config   [i_read].cols_1  ), .last_clk(lc_cols  ), .last(l_cols  ), .first(f_cols  ), .count(c_cols));
+  counter #(.W(BITS_IM_BLOCKS   )) C_IM_BLOCKS (.clk(aclk), .reset(copy_config), .en(lc_cols        ), .max_in(ref_config   [i_read].blocks_1), .last_clk(lc_blocks), .last(l_blocks)                                  );
 
-    - Enable
-      - At last_config, to accept (ref-1) for the first time
-      - At the last beat of smaller counter
-  */
+  // Last & User
 
-  assign en_count_kh        = m_axis_tvalid && m_axis_tready && (last_config || state_read == R_READ_S);
-  assign en_count_cin       = m_axis_tvalid && m_axis_tready && (last_config || (last_kh));
-  assign en_count_sw        = m_axis_tvalid && m_axis_tready && (last_config || (last_kh && last_cin)); // independant
-  assign en_count_cols      = m_axis_tvalid && m_axis_tready && (last_config || (last_kh && last_cin));
-  assign en_count_blocks    = m_axis_tvalid && m_axis_tready && (last_config || (last_kh && last_cin && last_cols));
+  assign m_axis_tlast = lc_blocks;
 
-  assign count_next_kh     = (last_kh       || last_config || ref_kh2      [i_read] == 0) ? 2*ref_kh2   [i_read] : count_kh     - 1;
-  assign count_next_cin    = (last_cin      || last_config || ref_1_cin    [i_read] == 0) ? ref_1_cin   [i_read] : count_cin    - 1;
-  assign count_next_sw     = (last_sw       || last_config || ref_1_sw     [i_read] == 0) ? ref_1_sw    [i_read] : count_sw     - 1;
-  assign count_next_cols   = (last_cols     || last_config || ref_1_cols   [i_read] == 0) ? ref_1_cols  [i_read] : count_cols   - 1;
-  assign count_next_blocks = (last_blocks   || last_config || ref_1_blocks [i_read] == 0) ? ref_1_blocks[i_read] : count_blocks - 1;
-
-  assign last_next_config  = count_next_config == 0;
-  assign last_next_kh      = count_next_kh     == 0 || ref_kh2      [i_read] == 0;
-  assign last_next_cin     = count_next_cin    == 0 || ref_1_cin    [i_read] == 0;
-  assign last_next_sw      = count_next_sw     == 0 || ref_1_sw     [i_read] == 0;
-  assign last_next_cols    = count_next_cols   == 0 || ref_1_cols   [i_read] == 0;
-  assign last_next_blocks  = count_next_blocks == 0 || ref_1_blocks [i_read] == 0;
-
-  /*
-    TLAST and TUSER
-  */
-
-  assign m_axis_tlast = last_kh && last_cin && last_cols && last_blocks;
-
-  assign m_axis_tuser.is_config    = state_read  == R_PASS_CONFIG_S;
-  assign m_axis_tuser.kw2          = ref_kw2  [i_read];
-  assign m_axis_tuser.sw_1         = ref_1_sw [i_read];
-  assign m_axis_tuser.is_w_first_clk   = count_cols == ref_1_cols[i_read] && count_cin == ref_1_cin[i_read] && count_kh == 2*ref_kh2 [i_read];
-  assign m_axis_tuser.is_cin_last  = (last_kh && last_cin);
-  assign m_axis_tuser.is_col_1_k2  = count_cols   == ref_kw2      [i_read]; // i = cols-1-k/2 === [cols-1-i] = k/2
-  assign m_axis_tuser.is_top_block = count_blocks == ref_1_blocks [i_read];
-  assign m_axis_tuser.is_bot_block = last_blocks;
-  assign m_axis_tuser.is_col_valid = count_sw == ref_1_sw [i_read] - (ref_1_sw [i_read] == 0  ? 0 : 1); // if no stride, si=0 else si=1
-  assign m_axis_tuser.is_sum_start = count_sw == ref_1_sw [i_read] - (ref_1_sw [i_read] == 2-1? 1 : 0); // if (7,2)    , si=1 else si=0
-  assign m_axis_tuser.is_w_first_kw2   = (ref_1_cols[i_read] - count_cols) < ref_kh2 [i_read];
-  assign m_axis_tuser.is_w_last        = count_cols == 0;
-
-  register #(
-    .WORD_WIDTH   (BITS_CONFIG_COUNT), 
-    .RESET_VALUE  (0)
-  ) COUNT_CONFIG (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .clock_enable (en_count_config),
-    .data_in      (count_next_config),
-    .data_out     (count_config)
-  );
-
-  register #(
-    .WORD_WIDTH   (1), 
-    .RESET_VALUE  (0)
-  ) LAST_CONFIG (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (last_next_config),
-    .clock_enable (en_count_config),
-    .data_out     (last_config)
-  );
-
-  register #(
-    .WORD_WIDTH   (BITS_KH), 
-    .RESET_VALUE  (0)
-  ) COUNT_KH (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (count_next_kh),
-    .clock_enable (en_count_kh),
-    .data_out     (count_kh)
-  );
-  register #(
-    .WORD_WIDTH   (1), 
-    .RESET_VALUE  (0)
-  ) LAST_KH (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (last_next_kh),
-    .clock_enable (en_count_kh),
-    .data_out     (last_kh)
-  );
-  register #(
-    .WORD_WIDTH   (BITS_IM_CIN), 
-    .RESET_VALUE  (0)
-  ) COUNT_CIN (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (count_next_cin),
-    .clock_enable (en_count_cin),
-    .data_out     (count_cin)
-  );
-  register #(
-    .WORD_WIDTH   (1), 
-    .RESET_VALUE  (0)
-  ) LAST_CIN (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (last_next_cin),
-    .clock_enable (en_count_cin),
-    .data_out     (last_cin)
-  );
-  register #(
-    .WORD_WIDTH   (BITS_SW), 
-    .RESET_VALUE  (0)
-  ) COUNT_SW (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (count_next_sw),
-    .clock_enable (en_count_sw),
-    .data_out     (count_sw)
-  );
-  register #(
-    .WORD_WIDTH   (1), 
-    .RESET_VALUE  (0)
-  ) LAST_SW (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (last_next_sw),
-    .clock_enable (en_count_sw),
-    .data_out     (last_sw)
-  );
-  register #(
-    .WORD_WIDTH   (BITS_IM_COLS), 
-    .RESET_VALUE  (0)
-  ) COUNT_COLS (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (count_next_cols),
-    .clock_enable (en_count_cols),
-    .data_out     (count_cols)
-  );
-  register #(
-    .WORD_WIDTH   (1), 
-    .RESET_VALUE  (0)
-  ) LAST_COLS (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (last_next_cols),
-    .clock_enable (en_count_cols),
-    .data_out     (last_cols)
-  );
-  register #(
-    .WORD_WIDTH   (BITS_IM_BLOCKS), 
-    .RESET_VALUE  (0)
-  ) COUNT_BLOCKS (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (count_next_blocks),
-    .clock_enable (en_count_blocks),
-    .data_out     (count_blocks)
-  );
-  register #(
-    .WORD_WIDTH   (1), 
-    .RESET_VALUE  (0)
-  ) LAST_BLOCKS (
-    .clock        (aclk),
-    .resetn       (aresetn),
-    .data_in      (last_next_blocks),
-    .clock_enable (en_count_blocks),
-    .data_out     (last_blocks)
-  );
+  assign m_axis_tuser.is_config        = state_read  == R_PASS_CONFIG_S;
+  assign m_axis_tuser.kw2              = ref_config  [i_read].kw2;
+  assign m_axis_tuser.sw_1             = 0;
+  assign m_axis_tuser.is_w_first_clk   = f_cols && f_cin && f_kw;
+  assign m_axis_tuser.is_cin_last      = l_kw   && l_cin;
+  assign m_axis_tuser.is_col_1_k2      = c_cols == ref_config [i_read].kw2; // i = cols-1-k/2 === [cols-1-i] = k/2
+  assign m_axis_tuser.is_col_valid     = 1; // if no stride, si=0 else si=1
+  assign m_axis_tuser.is_sum_start     = 1; // if (7,2)    , si=1 else si=0
+  assign m_axis_tuser.is_w_first_kw2   = (ref_config[i_read].cols_1 - c_cols) < ref_config[i_read].kw2;
+  assign m_axis_tuser.is_w_last        = l_cols;
 
 endmodule
-
