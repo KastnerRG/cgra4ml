4536563e8225ffcaf6833de084053bedee072254
Aba <abarajithan-7@gmail.com>
2023-05-04 14:57:57 -0700

    Remove input pipe wrapper

--- rtl/dnn_engine.v
+++ rtl/dnn_engine.v
@@ -40,55 +40,68 @@ module dnn_engine #(
 
   /* WIRES */
 
-  wire input_m_axis_tready;
-  wire input_m_axis_tvalid;
-  wire input_m_axis_tlast ;
-  wire [WORD_WIDTH*ROWS    -1:0] input_m_axis_pixels_tdata;
-  wire [WORD_WIDTH*COLS    -1:0] input_m_axis_weights_tdata;
-  wire [TUSER_WIDTH        -1:0] input_m_axis_tuser        ;
+  wire pixels_m_valid, pixels_m_ready;
+  wire weights_m_valid, weights_m_ready, weights_m_last;
+  wire conv_s_valid, conv_s_ready;
+  wire [WORD_WIDTH*ROWS    -1:0] pixels_m_data;
+  wire [WORD_WIDTH*COLS    -1:0] weights_m_data;
+  wire [TUSER_WIDTH        -1:0] weights_m_user;
 
-  wire conv_m_axis_tready;
-  wire conv_m_axis_tvalid;
-  wire conv_m_axis_tlast ;
+  wire conv_m_axis_tready, conv_m_axis_tvalid, conv_m_axis_tlast ;
   wire [TUSER_WIDTH          -1:0] conv_m_axis_tuser;
   wire [M_DATA_WIDTH_HF_CONV -1:0] conv_m_axis_tdata; // cgmu
 
-  wire dw_s_axis_tready;
-  wire dw_s_axis_tvalid;
-  wire dw_s_axis_tlast ;
+  wire dw_s_axis_tready, dw_s_axis_tvalid, dw_s_axis_tlast ;
   wire [TUSER_WIDTH    -1:0] dw_s_axis_tuser ;
   wire [M_DATA_WIDTH_HF_CONV_DW -1:0] dw_s_axis_tdata ;
 
-
-  axis_input_pipe input_pipe (
-    .aclk                      (aclk    ),
-    .aresetn                   (aresetn ),
-    .s_axis_pixels_tready      (s_axis_pixels_tready       ), 
-    .s_axis_pixels_tvalid      (s_axis_pixels_tvalid       ), 
-    .s_axis_pixels_tlast       (s_axis_pixels_tlast        ), 
-    .s_axis_pixels_tdata       (s_axis_pixels_tdata        ), 
-    .s_axis_pixels_tkeep       (s_axis_pixels_tkeep        ), 
-    .s_axis_weights_tready     (s_axis_weights_tready      ),
-    .s_axis_weights_tvalid     (s_axis_weights_tvalid      ),
-    .s_axis_weights_tlast      (s_axis_weights_tlast       ),
-    .s_axis_weights_tdata      (s_axis_weights_tdata       ),
-    .s_axis_weights_tkeep      (s_axis_weights_tkeep       ),
-    .m_axis_tready             (input_m_axis_tready        ),      
-    .m_axis_tvalid             (input_m_axis_tvalid        ),     
-    .m_axis_tlast              (input_m_axis_tlast         ),     
-    .m_axis_pixels_tdata       (input_m_axis_pixels_tdata  ),
-    .m_axis_weights_tdata      (input_m_axis_weights_tdata ), // CMG_flat
-    .m_axis_tuser              (input_m_axis_tuser         )
+  axis_pixels PIXELS (
+    .aclk   (aclk   ),
+    .aresetn(aresetn),
+    .s_ready(s_axis_pixels_tready),
+    .s_valid(s_axis_pixels_tvalid),
+    .s_last (s_axis_pixels_tlast ),
+    .s_data (s_axis_pixels_tdata ),
+    .s_keep (s_axis_pixels_tkeep ),
+    .m_valid(pixels_m_valid      ),
+    .m_ready(pixels_m_ready      ),
+    .m_data (pixels_m_data       )
   );
+
+  axis_weight_rotator WEIGHTS_ROTATOR (
+    .aclk          (aclk                 ),
+    .aresetn       (aresetn              ),
+    .s_axis_tready (s_axis_weights_tready), 
+    .s_axis_tvalid (s_axis_weights_tvalid), 
+    .s_axis_tlast  (s_axis_weights_tlast ), 
+    .s_axis_tdata  (s_axis_weights_tdata ),
+    .s_axis_tkeep  (s_axis_weights_tkeep ),
+    .m_axis_tready (weights_m_ready      ),      
+    .m_axis_tvalid (weights_m_valid      ),   
+    .m_axis_tdata  (weights_m_data       ),
+    .m_axis_tlast  (weights_m_last       ),
+    .m_axis_tuser  (weights_m_user       ) 
+  );
+
+  axis_sync SYNC (
+    .weights_m_valid (weights_m_valid), 
+    .pixels_m_valid  (pixels_m_valid ), 
+    .m_axis_tready   (conv_s_ready   ),
+    .weights_m_user  (weights_m_user ),
+    .m_axis_tvalid   (conv_s_valid   ), 
+    .weights_m_ready (weights_m_ready), 
+    .pixels_m_ready  (pixels_m_ready ) 
+  );
+
   proc_engine PROC_ENGINE (
     .clk            (aclk    ),
     .resetn         (aresetn ),
-    .s_valid        (input_m_axis_tvalid        ),
-    .s_ready        (input_m_axis_tready        ),
-    .s_last         (input_m_axis_tlast         ),
-    .s_user         (input_m_axis_tuser         ),
-    .s_data_pixels  (input_m_axis_pixels_tdata  ),
-    .s_data_weights (input_m_axis_weights_tdata ),
+    .s_valid        (conv_s_valid               ),
+    .s_ready        (conv_s_ready               ),
+    .s_last         (weights_m_last             ),
+    .s_user         (weights_m_user             ),
+    .s_data_pixels  (pixels_m_data              ),
+    .s_data_weights (weights_m_data             ),
     .m_valid        (conv_m_axis_tvalid         ),
     .m_ready        (conv_m_axis_tready         ),
     .m_data         (conv_m_axis_tdata          ),
