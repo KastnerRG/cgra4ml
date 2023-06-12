8892ccc29bf23e3bf7dd0d4426b2bdc044e9e490
Aba <abarajithan-7@gmail.com>
2023-06-11 21:24:58 -0700

    Refactor proc_engine, update Latency MUL=2

--- rtl/proc_engine.sv
+++ rtl/proc_engine.sv
@@ -39,7 +39,7 @@ module proc_engine #(
   logic [COLS-1:0][ROWS -1:0][WORD_WIDTH_OUT-1:0] i_data;
   tuser_st i_user;
 
-  logic clken_mul, mux_sel_next, mux_sel, mul_m_valid, acc_m_valid_next, acc_m_valid, mul_m_last, acc_m_last;
+  logic clken_mul, sel_shift_next, sel_shift, mul_m_valid, acc_m_valid_next, acc_m_valid, mul_m_last, acc_m_last;
   logic [COLS   -1: 0] clken_acc, bypass_sum, bypass_sum_next, bypass;
   logic [COLS   -1: 0] acc_m_sum_start, acc_s_valid, acc_m_keep;
   tuser_st mul_m_user, acc_s_user, mux_s2_user, acc_m_user;
@@ -47,74 +47,71 @@ module proc_engine #(
   logic [COLS    -1:0] lut_sum_start [KW_MAX/2:0][SW_MAX -1:0];
 
   logic [WORD_WIDTH_IN*2-1:0] mul_m_data  [COLS   -1:0][ROWS -1:0];
-  logic [WORD_WIDTH_OUT -1:0] mux_s2_data [COLS   -1:0][ROWS -1:0];
+  logic [WORD_WIDTH_OUT -1:0] shift_data  [COLS   -1:0][ROWS -1:0];
 
   assign s_ready = clken_mul;
 
   generate
-    genvar u,m,b,kw2,sw_1;
+    genvar r,c,kw2,sw_1;
     n_delay #(.N(LATENCY_MULTIPLIER), .W(TUSER_WIDTH+2)) MUL_CONTROL (.c(clk), .rn(resetn), .e(clken_mul), .i({s_valid, s_last, s_user}), .o ({mul_m_valid, mul_m_last, mul_m_user}));
 
-        for (u=0; u < ROWS ; u++)
-          for (m=0; m < COLS   ; m++)
-            if (m==0) assign mux_s2_data [m][u] = 0;
-            else      assign mux_s2_data [m][u] = i_data     [m-1][u];
-
-    assign mux_sel_next = mul_m_valid && mul_m_user.is_cin_last && (mul_m_user.kw2 != 0);
+    assign sel_shift_next = mul_m_valid && mul_m_user.is_cin_last && (mul_m_user.kw2 != 0);
 
     always_ff @(posedge clk)
-      if (!resetn) mux_sel <= 0;
-      else if (en) mux_sel <= mux_sel_next;
+      if (!resetn) sel_shift <= 0;
+      else if (en) sel_shift <= sel_shift_next;
 
-    assign clken_mul = en    && !mux_sel;
+    assign clken_mul = en  && !sel_shift;
             
-    for (m=0; m < COLS   ; m++) begin: Mb
+    for (c=0; c < COLS   ; c++) begin: Mb
       for (kw2=0; kw2 <= KW_MAX/2; kw2++)
         for (sw_1=0; sw_1 < SW_MAX; sw_1++) begin
           localparam k = kw2*2 + 1;
           localparam s = sw_1 + 1;
           localparam j = k + s -1;
 
-          assign lut_sum_start[kw2][sw_1][m] = m % j < s; // m % 3 < 1 : 0,1
+          assign lut_sum_start[kw2][sw_1][c] = c % j < s; // c % 3 < 1 : 0,1
         end
       
-      assign acc_m_sum_start [m] = lut_sum_start[acc_m_user.kw2][acc_m_user.sw_1][m] & acc_m_user.is_sum_start;
-      assign acc_s_valid     [m] = mux_sel ? ~acc_m_sum_start [m] : mul_m_valid;
+      assign acc_m_sum_start [c] = lut_sum_start[acc_m_user.kw2][acc_m_user.sw_1][c];
+      assign acc_s_valid     [c] = sel_shift ? ~acc_m_sum_start [c] : mul_m_valid;
+      assign clken_acc       [c] = en    && acc_s_valid [c];
 
-      assign bypass_sum_next [m] = mul_m_user.is_cin_last || mul_m_user.is_config;
+      assign bypass_sum_next [c] = mul_m_user.is_cin_last || mul_m_user.is_config;
 
       always_ff @(posedge clk)
-        if (!resetn)                    bypass_sum [m] <= 0;
-        else if (en && acc_s_valid [m]) bypass_sum [m] <= bypass_sum_next [m];
+        if (!resetn)            bypass_sum [c] <= 0;
+        else if (clken_acc [c]) bypass_sum [c] <= bypass_sum_next [c];
 
-      assign bypass    [m] = bypass_sum [m] || mul_m_user.is_w_first_clk; // clears all partial sums for every first col
-      assign clken_acc [m] = en    && acc_s_valid [m];
+      assign bypass    [c] = bypass_sum [c] || mul_m_user.is_w_first_clk; // clears all partial sums for every first col
+
     end
 
-        for (u=0; u < ROWS ; u++) begin: Ua
-          for (m=0; m < COLS   ; m++) begin: Ma
-            proc_element #(
-              .WORD_WIDTH_IN (WORD_WIDTH_IN ),
-              .WORD_WIDTH_OUT(WORD_WIDTH_OUT)
-              ) PE (
-              .clk           (clk           ),
-              .clken         (en            ),
-              .resetn        (resetn        ),
-              .clken_mul     (clken_mul     ),
-              .s_data_pixels (s_data_pixels    [u]), 
-              .s_data_weights(s_data_weights[m]   ),
-              .mul_m_data    (mul_m_data    [m][u]),
-              .mux_sel       (mux_sel       ),
-              .mux_s2_data   (mux_s2_data   [m][u]),
-              .bypass        (bypass        [m]),
-              .clken_acc     (clken_acc     [m]),
-              .m_data        (i_data        [m][u])
-            );
+    for (r=0; r < ROWS ; r++) begin: Ua
+      for (c=0; c < COLS   ; c++) begin: Ma
+        assign shift_data [c][r] = c==0 ? 0 : i_data [c-1][r];
+        proc_element #(
+          .WORD_WIDTH_IN (WORD_WIDTH_IN ),
+          .WORD_WIDTH_OUT(WORD_WIDTH_OUT)
+          ) PE (
+          .clk           (clk           ),
+          .clken         (en            ),
+          .resetn        (resetn        ),
+          .clken_mul     (clken_mul     ),
+          .sel_shift     (sel_shift     ),
+          .s_data_pixels (s_data_pixels    [r]), 
+          .s_data_weights(s_data_weights[c]   ),
+          .mul_m_data    (mul_m_data    [c][r]),
+          .shift_data    (shift_data    [c][r]),
+          .bypass        (bypass        [c]),
+          .clken_acc     (clken_acc     [c]),
+          .m_data        (i_data        [c][r])
+        );
     end end
 
     n_delay #(.N(LATENCY_ACCUMULATOR), .W(TUSER_WIDTH)) ACC_USER (.c(clk), .rn(resetn), .e(en & mul_m_valid), .i(mul_m_user), .o(acc_m_user));
 
-    assign acc_m_valid_next = !mux_sel && mul_m_valid && (mul_m_user.is_config || mul_m_user.is_cin_last);
+    assign acc_m_valid_next = !sel_shift && mul_m_valid && (mul_m_user.is_config || mul_m_user.is_cin_last);
     
     n_delay #(.N(LATENCY_ACCUMULATOR), .W(2)) ACC_VALID_LAST(.c(clk), .rn(resetn), .e(en), .i({acc_m_valid_next, mul_m_last}), .o({acc_m_valid, acc_m_last}));
 
