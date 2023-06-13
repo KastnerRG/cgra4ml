a83bc16b47ae905fe037c70921a77b011155e057
Aba <abarajithan-7@gmail.com>
2023-06-13 11:31:38 -0700

    Change array to 7x96

--- fpga/scripts/vivado_config.tcl
+++ fpga/scripts/vivado_config.tcl
@@ -1,9 +1,9 @@
 
     # Written from param_tests.py
     set RAM_WEIGHTS_DEPTH 2049
-    set COLS               25
+    set COLS               96
     set X_BITS             8
-    set K_BITS             4
+    set K_BITS             8
     set DELAY_W_RAM        2
     set RAM_EDGES_DEPTH    672
     set KH_MAX             11
