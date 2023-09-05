d0fdae3b9055e9f45fa56bb53b39371b738dc19c
Aba <abarajithan-7@gmail.com>
2023-09-04 19:26:25 -0700

    Update counters in callback with all indices

--- c/model.h
+++ c/model.h
@@ -1,17 +1,20 @@
 #define N_BUNDLES 6
 Bundle_t bundles [N_BUNDLES] = {
-   {.w_bpt=152, .w_bpt_p0=152, .x_bpt=216, .x_bpt_p0=216, .y_wpt=16, .y_wpt_last=96, .y_nl=4, .y_w=3, .n_it=8, .n_p=3, .x_header=414341061322735616, .x_header_p0=414341061322735616, .w_header=414376340184104960, .w_header_p0=414341061322735616 },
-   {.w_bpt=188, .w_bpt_p0=188, .x_bpt=424, .x_bpt_p0=424, .y_wpt=24, .y_wpt_last=96, .y_nl=4, .y_w=5, .n_it=6, .n_p=8, .x_header=846686625550303232, .x_header_p0=846686625550303232, .w_header=846721930181476352, .w_header_p0=846686625550303232 },
-   {.w_bpt=200, .w_bpt_p0=80, .x_bpt=632, .x_bpt_p0=216, .y_wpt=32, .y_wpt_last=96, .y_nl=4, .y_w=6, .n_it=4, .n_p=6, .x_header=1351089783815798784, .x_header_p0=198168279208951808, .w_header=1351125097036906496, .w_header_p0=198168279208951808 },
-   {.w_bpt=200, .w_bpt_p0=56, .x_bpt=1048, .x_bpt_p0=216, .y_wpt=64, .y_wpt_last=128, .y_nl=4, .y_w=7, .n_it=3, .n_p=4, .x_header=2431953694384717824, .x_header_p0=126110685171023872, .w_header=2431989007605825536, .w_header_p0=126110685171023872 },
-   {.w_bpt=200, .w_bpt_p0=128, .x_bpt=3128, .x_bpt_p0=1880, .y_wpt=192, .y_wpt_last=192, .y_nl=4, .y_w=8, .n_it=3, .n_p=2, .x_header=8124503623381024768, .x_header_p0=4665739109560483840, .w_header=8124538936602132480, .w_header_p0=4665739109560483840 },
-   {.w_bpt=200, .w_bpt_p0=140, .x_bpt=203, .x_bpt_p0=138, .y_wpt=192, .y_wpt_last=192, .y_nl=2, .y_w=1, .n_it=1, .n_p=427, .x_header=8070450532247928832, .x_header_p0=5188146770730811392, .w_header=8070485845469036544, .w_header_p0=5188146770730811392 }
+   {.w_bpt=152, .w_bpt_p0=152, .x_bpt=216, .x_bpt_p0=216, .n=2, .l=2, .kw=11, .coe=2, .coe_tl=2, .r_ll=8, .h=16, .w=8, .w_kw2=3, .t=8, .p=3, .cm=1, .cm_p0=1, .x_header=414341061322735616, .x_header_p0=414341061322735616, .w_header=414376340184104960, .w_header_p0=414341061322735616 },
+   {.w_bpt=188, .w_bpt_p0=188, .x_bpt=424, .x_bpt_p0=424, .n=2, .l=2, .kw=7, .coe=3, .coe_tl=4, .r_ll=8, .h=16, .w=8, .w_kw2=5, .t=6, .p=8, .cm=2, .cm_p0=2, .x_header=846686625550303232, .x_header_p0=846686625550303232, .w_header=846721930181476352, .w_header_p0=846686625550303232 },
+   {.w_bpt=200, .w_bpt_p0=80, .x_bpt=632, .x_bpt_p0=216, .n=2, .l=2, .kw=5, .coe=4, .coe_tl=4, .r_ll=8, .h=16, .w=8, .w_kw2=6, .t=4, .p=6, .cm=3, .cm_p0=1, .x_header=1351089783815798784, .x_header_p0=198168279208951808, .w_header=1351125097036906496, .w_header_p0=198168279208951808 },
+   {.w_bpt=200, .w_bpt_p0=56, .x_bpt=1048, .x_bpt_p0=216, .n=2, .l=2, .kw=3, .coe=8, .coe_tl=8, .r_ll=8, .h=16, .w=8, .w_kw2=7, .t=3, .p=4, .cm=5, .cm_p0=1, .x_header=2431953694384717824, .x_header_p0=126110685171023872, .w_header=2431989007605825536, .w_header_p0=126110685171023872 },
+   {.w_bpt=200, .w_bpt_p0=128, .x_bpt=3128, .x_bpt_p0=1880, .n=2, .l=2, .kw=1, .coe=24, .coe_tl=2, .r_ll=8, .h=16, .w=8, .w_kw2=8, .t=3, .p=2, .cm=15, .cm_p0=9, .x_header=8124503623381024768, .x_header_p0=4665739109560483840, .w_header=8124538936602132480, .w_header_p0=4665739109560483840 },
+   {.w_bpt=200, .w_bpt_p0=140, .x_bpt=203, .x_bpt_p0=138, .n=2, .l=1, .kw=1, .coe=24, .coe_tl=0, .r_ll=1, .h=1, .w=1, .w_kw2=1, .t=1, .p=427, .cm=15, .cm_p0=10, .x_header=8070450532247928832, .x_header_p0=5188146770730811392, .w_header=8070485845469036544, .w_header_p0=5188146770730811392 }
 };
 
-#define X_BITS      4
-#define W_BITS      4
+#define X_BITS_L2   2
+#define W_BITS_L2   2
+#define PE_ROWS     8
+#define PE_COLS     24
+
 #define W_BYTES     105284
 #define X_BYTES     648
 #define X_BYTES_ALL 102400
-const char DATA_DIR [] = "D:/dnn-engine/test/vectors";
+#define DATA_DIR   "D:/dnn-engine/test/vectors"
 
