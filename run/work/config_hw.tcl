9632f5384837bb223e83bbeb1aa35cca8f0dc064
Aba <abarajithan-7@gmail.com>
2023-11-18 00:16:15 -0800

    Integrate Hardware class into param_test.py

--- run/work/config_hw.tcl
+++ run/work/config_hw.tcl
@@ -1,15 +1,15 @@
 
-    # Written from param_tests.py
-    set RAM_WEIGHTS_DEPTH 20
-    set ROWS               8
-    set COLS               24
-    set X_BITS             4
-    set K_BITS             4
-    set Y_BITS             24
-    set DELAY_W_RAM        2
-    set RAM_EDGES_DEPTH    288
-    set KH_MAX             13
-    set S_WEIGHTS_WIDTH_LF  64
-    set S_PIXELS_WIDTH_LF   64
-    set M_OUTPUT_WIDTH_LF   64
-        
+# Written from Hardware.export()
+                    
+set RAM_WEIGHTS_DEPTH  20
+set ROWS               8
+set COLS               24
+set X_BITS             4
+set K_BITS             4
+set Y_BITS             24
+set DELAY_W_RAM        2
+set RAM_EDGES_DEPTH    288
+set KH_MAX             13
+set S_WEIGHTS_WIDTH_LF 64
+set S_PIXELS_WIDTH_LF  64
+set M_OUTPUT_WIDTH_LF  64
