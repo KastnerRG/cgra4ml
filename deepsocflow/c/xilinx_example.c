cddc577fbf53c7c8de3aa620125d6df1bda8768f
ZhenghuaMa <54973815+zhenghuama@users.noreply.github.com>
2024-07-18 21:47:22 -0700

    Update xilinx_example.c
    fixed a typo

--- deepsocflow/c/xilinx_example.c
+++ deepsocflow/c/xilinx_example.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    hardware_setup()
+    hardware_setup();
 
     xil_printf("Welcome to DeepSoCFlow!\n Store wbx at: %p; y:%p; buffers {0:%p,1:%p}; debug_nhwc:%p; debug_tiled:%p \n", &mem.w, &mem.y, &mem.out_buffers[0], &mem.out_buffers[1], &mem.debug_nhwc, &mem.debug_tiled);
 
