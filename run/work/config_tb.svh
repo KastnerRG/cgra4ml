2f9c26f107a92b76d5318897724c03f89a57f646
Aba <abarajithan-7@gmail.com>
2025-02-07 14:27:00 -0800

    Minor changes

--- run/work/config_tb.svh
+++ run/work/config_tb.svh
@@ -1,6 +1,6 @@
 
 `define VALID_PROB 1000 
 `define READY_PROB 1000 
-`define CLK_PERIOD 4.0 
-`define INPUT_DELAY_NS  0.8ns
-`define OUTPUT_DELAY_NS 0.8ns
+`define CLK_PERIOD 6.7 
+`define INPUT_DELAY_NS  1.3ns
+`define OUTPUT_DELAY_NS 1.3ns
