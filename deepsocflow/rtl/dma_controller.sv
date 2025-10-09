f176a3d353d1a19bb6a88d191a612c404218dbe5
Aba <abarajithan-7@gmail.com>
2025-10-08 17:52:22 -0700

    Make reg-read zero cycle in controller

--- deepsocflow/rtl/dma_controller.sv
+++ deepsocflow/rtl/dma_controller.sv
@@ -81,12 +81,14 @@ module dma_controller #(
     ; // Max 16 registers
   (* mark_debug = "true" *) logic [12:0][AXI_DATA_WIDTH-1:0] cfg ;
 
-  always_ff @(posedge clk)  // PS READ (1 clock latency)
-    if (!rstn)          {reg_rd_data} <= '0;
-    else if (reg_rd_en) begin
-      //reg_rd_ack  <= reg_rd_en;
-      reg_rd_data <= cfg[reg_rd_addr];
-    end
+  // always_ff @(posedge clk)  // PS READ (1 clock latency)
+  //   if (!rstn)          {reg_rd_data} <= '0;
+  //   else if (reg_rd_en) begin
+  //     //reg_rd_ack  <= reg_rd_en;
+  //     reg_rd_data <= cfg[reg_rd_addr];
+  //   end
+
+  assign reg_rd_data = cfg[reg_rd_addr];
 
   assign reg_wr_ack = 1'b1;
   assign reg_rd_ack = 1'b1;
