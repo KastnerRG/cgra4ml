7eb9647a7c94f6b173a02aa9f3741316e5d9efdc
RaviduHM99 <raviduhm@gmail.com>
2025-10-17 14:20:07 +0530

    changes path without clashing with new repo

--- deepsocflow/tcl/asic/cadence/libraries/cadence.srams.TSMC28.tcl
+++ deepsocflow/tcl/asic/cadence/libraries/cadence.srams.TSMC28.tcl
@@ -1,5 +1,5 @@
 # Tech Paths of SRAM
-set paths(SRAM_TECH_FILES) "$paths(PDK_ROOT)/SRAM_Inst"
+set paths(SRAM_TECH_FILES) "$paths(PDK_ROOT)/SRAM_Inst_old_cgra4ml"
 
 set tech_files(SRAM_WEIGHTS_LEF) "$paths(SRAM_TECH_FILES)/sram_weights/sram_weights.lef"
     lappend tech_files(ALL_LEFS) $tech_files(SRAM_WEIGHTS_LEF)
--- deepsocflow/tcl/asic/cadence/scripts/tsmc.28.srams.tcl
+++ deepsocflow/tcl/asic/cadence/scripts/tsmc.28.srams.tcl
@@ -9,21 +9,21 @@ set SRAM_WEIGHT_MUX 2
 set SRAM_EDGES_EST_DEPTH   4096 
 set SRAM_WEIGHTS_EST_DEPTH $RAM_WEIGHTS_DEPTH
 
-set CORNERS ffg_cbestt_1p05v_1p05v_m40c,ffg_cbestt_0p88v_0p99v_m40c,ssg_cworstt_0p81v_0p81v_125c,tt_ctypical_0p80v_0p90v_85c,ssg_cworstt_0p72v_0p81v_125c,tt_ctypical_0p90v_0p90v_85c,ssg_cworstt_0p90v_0p90v_125c,tt_ctypical_1p00v_1p00v_85c,ffg_cbestt_0p99v_0p99v_m40c
+set CORNERS "ffg_cbestt_1p05v_1p05v_m40c,ffg_cbestt_0p88v_0p99v_m40c,ssg_cworstt_0p81v_0p81v_125c,tt_ctypical_0p80v_0p90v_85c,ssg_cworstt_0p72v_0p81v_125c,tt_ctypical_0p90v_0p90v_85c,ssg_cworstt_0p90v_0p90v_125c,tt_ctypical_1p00v_1p00v_85c,ffg_cbestt_0p99v_0p99v_m40c"
 
 # Generate Folders
-exec mkdir -p ../../../PDK/TSMC28HPCPLUS/SRAM_Inst
-exec mkdir -p ../../../PDK/TSMC28HPCPLUS/SRAM_Inst/sram_edges
-exec mkdir -p ../../../PDK/TSMC28HPCPLUS/SRAM_Inst/sram_weights
+exec mkdir -p ../../../PDK/TSMC28HPCPLUS/SRAM_Inst_old_cgra4ml
+exec mkdir -p ../../../PDK/TSMC28HPCPLUS/SRAM_Inst_old_cgra4ml/sram_edges
+exec mkdir -p ../../../PDK/TSMC28HPCPLUS/SRAM_Inst_old_cgra4ml/sram_weights
 
 #Genrating SRAM EDGES
-cd ../../../PDK/TSMC28HPCPLUS/SRAM_Inst/sram_edges
-exec rf_sp_hde_hvt_mvt all -activity_factor 50 -back_biasing off -bits $EDGE_BITS -bmux off -bus_notation on -check_instname on -corners $CORNERS -cust_comment "" -diodes on -drive 6 -ema on -frequency $FREQ -instname sram_edges -left_bus_delim {[} -libertyviewstyle nldm -libname SRAM_EDGES -mux $SRAM_EDGE_MUX -name_case upper -power_type otc -prefix "" -pwr_gnd_rename vddpe:VDD,vddce:VDD,vsse:VSS -retention on -right_bus_delim {]} -ser none -site_def off -top_layer m5-m10 -words $SRAM_EDGES_EST_DEPTH -writemask off -write_thru off -power_gating off -redundancy off
-exec rf_sp_hde_hvt_mvt liberty -activity_factor 50 -back_biasing off -bits $EDGE_BITS -bmux off -bus_notation on -check_instname on -corners $CORNERS -cust_comment "" -diodes on -drive 6 -ema on -frequency $FREQ -instname sram_edges -left_bus_delim {[} -libertyviewstyle nldm -libname SRAM_EDGES -mux $SRAM_EDGE_MUX -name_case upper -power_type otc -prefix "" -pwr_gnd_rename vddpe:VDD,vddce:VDD,vsse:VSS -retention on -right_bus_delim {]} -ser none -site_def off -top_layer m5-m10 -words $SRAM_EDGES_EST_DEPTH -writemask off -write_thru off -power_gating off -redundancy off
+cd ../../../PDK/TSMC28HPCPLUS/SRAM_Inst_old_cgra4ml/sram_edges
+exec rf_sp_hde_hvt_mvt all -activity_factor 50 -back_biasing off -bits $EDGE_BITS -bmux off -bus_notation on -check_instname on -corners $CORNERS -cust_comment "" -diodes on -drive 6 -ema on -frequency $FREQ -instname sram_edges -left_bus_delim {[} -libertyviewstyle nldm -libname SRAM_EDGES -mux $SRAM_EDGE_MUX -name_case upper -power_type otc -prefix "" -pwr_gnd_rename vddpe:VDD,vddce:VDD,vsse:VSS -retention on -right_bus_delim {]} -ser none -site_def off -top_layer m5-m10 -words $SRAM_EDGES_EST_DEPTH -write_mask off -write_thru off -power_gating off -redundancy off
+exec rf_sp_hde_hvt_mvt liberty -activity_factor 50 -back_biasing off -bits $EDGE_BITS -bmux off -bus_notation on -check_instname on -corners $CORNERS -cust_comment "" -diodes on -drive 6 -ema on -frequency $FREQ -instname sram_edges -left_bus_delim {[} -libertyviewstyle nldm -libname SRAM_EDGES -mux $SRAM_EDGE_MUX -name_case upper -power_type otc -prefix "" -pwr_gnd_rename vddpe:VDD,vddce:VDD,vsse:VSS -retention on -right_bus_delim {]} -ser none -site_def off -top_layer m5-m10 -words $SRAM_EDGES_EST_DEPTH -write_mask off -write_thru off -power_gating off -redundancy off
 cd ../../../../cgra4ml/run/work
 
 #Genrating SRAM WEIGHTS
-cd ../../../PDK/TSMC28HPCPLUS/SRAM_Inst/sram_weights
-exec rf_sp_hde_hvt_mvt all -activity_factor 50 -back_biasing off -bits $WEIGHT_BITS -bmux off -bus_notation on -check_instname on -corners $CORNERS -cust_comment "" -diodes on -drive 6 -ema on -frequency $FREQ -instname sram_weights -left_bus_delim {[} -libertyviewstyle nldm -libname SRAM_WEIGHTS -mux $SRAM_WEIGHT_MUX -name_case upper -power_type otc -prefix "" -pwr_gnd_rename vddpe:VDD,vddce:VDD,vsse:VSS -retention on -right_bus_delim {]} -ser none -site_def off -top_layer m5-m10 -words $SRAM_WEIGHTS_EST_DEPTH -writemask off -write_thru off -power_gating off -redundancy off
-exec rf_sp_hde_hvt_mvt liberty -activity_factor 50 -back_biasing off -bits $WEIGHT_BITS -bmux off -bus_notation on -check_instname on -corners $CORNERS -cust_comment "" -diodes on -drive 6 -ema on -frequency $FREQ -instname sram_weights -left_bus_delim {[} -libertyviewstyle nldm -libname SRAM_WEIGHTS -mux $SRAM_WEIGHT_MUX -name_case upper -power_type otc -prefix "" -pwr_gnd_rename vddpe:VDD,vddce:VDD,vsse:VSS -retention on -right_bus_delim {]} -ser none -site_def off -top_layer m5-m10 -words $SRAM_WEIGHTS_EST_DEPTH -writemask off -write_thru off -power_gating off -redundancy off
+cd ../../../PDK/TSMC28HPCPLUS/SRAM_Inst_old_cgra4ml/sram_weights
+exec rf_sp_hde_hvt_mvt all -activity_factor 50 -back_biasing off -bits $WEIGHT_BITS -bmux off -bus_notation on -check_instname on -corners $CORNERS -cust_comment "" -diodes on -drive 6 -ema on -frequency $FREQ -instname sram_weights -left_bus_delim {[} -libertyviewstyle nldm -libname SRAM_WEIGHTS -mux $SRAM_WEIGHT_MUX -name_case upper -power_type otc -prefix "" -pwr_gnd_rename vddpe:VDD,vddce:VDD,vsse:VSS -retention on -right_bus_delim {]} -ser none -site_def off -top_layer m5-m10 -words $SRAM_WEIGHTS_EST_DEPTH -write_mask off -write_thru off -power_gating off -redundancy off
+exec rf_sp_hde_hvt_mvt liberty -activity_factor 50 -back_biasing off -bits $WEIGHT_BITS -bmux off -bus_notation on -check_instname on -corners $CORNERS -cust_comment "" -diodes on -drive 6 -ema on -frequency $FREQ -instname sram_weights -left_bus_delim {[} -libertyviewstyle nldm -libname SRAM_WEIGHTS -mux $SRAM_WEIGHT_MUX -name_case upper -power_type otc -prefix "" -pwr_gnd_rename vddpe:VDD,vddce:VDD,vsse:VSS -retention on -right_bus_delim {]} -ser none -site_def off -top_layer m5-m10 -words $SRAM_WEIGHTS_EST_DEPTH -write_mask off -write_thru off -power_gating off -redundancy off
 cd ../../../../cgra4ml/run/work
