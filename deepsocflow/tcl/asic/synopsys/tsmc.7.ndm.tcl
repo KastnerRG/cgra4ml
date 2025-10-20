4324ee6071b8196dd580375f84364d0aa3b6cec3
RaviduHM99 <raviduhm@gmail.com>
2025-10-20 12:48:49 +0530

    change ndm libs

--- deepsocflow/tcl/asic/synopsys/tsmc.7.ndm.tcl
+++ deepsocflow/tcl/asic/synopsys/tsmc.7.ndm.tcl
@@ -1,7 +1,8 @@
 set metal_stack         1p13m_1x1xa1ya5y2yy2z
 set ndm_sram_library    tsmc_7_srams.ndm
-set ndmtfPath 	       "../asic/pdk/tsmc7/ndm/$metal_stack"
-set sramLibPath        "../asic/srams"
-set search_path        [concat $search_path $sramLibPath/sram_weights $sramLibPath/sram_edges] 
+set lef 	            "../asic/pdk/tsmc7/lef"
+set ndmtfPath 	        "../asic/pdk/tsmc7/ndm/$metal_stack"
+set sramLibPath         "../asic/srams"
+set search_path         [concat $search_path $sramLibPath/sram_weights $sramLibPath/sram_edges] 
 
-create_lib -ref_libs [list sram_weights.lef sram_edges.lef] -technology $ndmtfPath/sch240mc_tech.tf $sramLibPath/$ndm_sram_library
+create_lib -ref_libs [list $lef/sch240mc_cln07ff41001_base_svt_c11.lef sram_weights.lef sram_edges.lef] -technology $ndmtfPath/sch240mc_tech.tf $sramLibPath/$ndm_sram_library
