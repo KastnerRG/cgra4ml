c7b0149a3520945d22cc1801c55f9383eb7627a0
RaviduHM99 <raviduhm@gmail.com>
2025-10-20 13:37:06 +0530

    syntax error solved

--- deepsocflow/tcl/asic/synopsys/tsmc.7.ndm.tcl
+++ deepsocflow/tcl/asic/synopsys/tsmc.7.ndm.tcl
@@ -1,8 +1,9 @@
 set metal_stack         1p13m_1x1xa1ya5y2yy2z
+set ndm_sram_library    tsmc_7_srams.ndm
 
 set ndmtfPath 	        "../asic/pdk/tsmc7/ndm/$metal_stack"
 set sramLibPath         "../asic/srams"
 set search_path         [concat $search_path $sramLibPath/sram_weights $sramLibPath/sram_edges] 
 
 set_app_var link_library "sram_edges_ssgnp_cworstccworstt_0p90v_0p90v_125c.db sram_weights_ssgnp_cworstccworstt_0p90v_0p90v_125c.db"
-create_lib -ref_libs [list sram_weights.lef sram_edges.lef] -technology $ndmtfPath/sch240mc_tech.tf
+create_lib -ref_libs [list sram_weights.lef sram_edges.lef] -technology $ndmtfPath/sch240mc_tech.tf $ndm_sram_library
