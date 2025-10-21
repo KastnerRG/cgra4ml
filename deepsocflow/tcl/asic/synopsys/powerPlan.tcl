8d75a8003738979cdcd6da84eac576c7d20e380b
RaviduHM99 <raviduhm@gmail.com>
2025-10-21 16:44:02 +0530

    error solved in power plan script

--- deepsocflow/tcl/asic/synopsys/powerPlan.tcl
+++ deepsocflow/tcl/asic/synopsys/powerPlan.tcl
@@ -5,33 +5,35 @@ set_attribute [get_lib_cells */*TIE*] dont_touch false
 set_lib_cell_purpose -include optimization [get_lib_cells */*TIE*]
 
 # Create rails for macros
-create_pg_ring_pattern ring_pattern -horizontal_layer M12 -horizontal_width {1} -horizontal_spacing {1} -vertical_layer M13 -vertical_width {1} -vertical_spacing {1}
-set_pg_strategy sram_ring -macros { PIXELS_RAM_genblk1_0__RAME \
-	  WEIGHTS_ROTATOR_genblk1_0__BRAM_BRAM_genblk1_0__RAMW \
-	  WEIGHTS_ROTATOR_genblk1_0__BRAM_BRAM_genblk1_1__RAMW } -pattern {{name : ring_pattern} {nets: {VDD VSS}}{offset: {4 4}}}
+create_pg_ring_pattern sram_ring_patt -horizontal_layer M12 -horizontal_width {1} -horizontal_spacing {1} -vertical_layer M13 -vertical_width {1} -vertical_spacing {1}
+set_pg_strategy sram_ring -macros { PIXELS_RAM_RAME \
+	WEIGHTS_ROTATOR_genblk1_0__BRAM_BRAM_RAMW \
+	WEIGHTS_ROTATOR_genblk1_1__BRAM_BRAM_RAMW} -pattern {{name : sram_ring_patt} {nets: {VDD VSS}}{offset: {4 4}}}
 
 create_pg_macro_conn_pattern sram_pg_mesh -pin_conn_type long_pin -nets {VDD VSS} -direction horizontal -layers M4 -width 0.5 -spacing interleaving -pitch 5 -pin_layers {M4} -via_rule {{intersection : all}}
-set_pg_strategy sram_pg_mesh -macros { PIXELS_RAM_genblk1_0__RAME \
-	  WEIGHTS_ROTATOR_genblk1_0__BRAM_BRAM_genblk1_0__RAMW \
-	  WEIGHTS_ROTATOR_genblk1_0__BRAM_BRAM_genblk1_1__RAMW } -pattern {{name : sram_pg_mesh} {nets : {VDD VSS}}}
+set_pg_strategy sram_pg_mesh -macros { PIXELS_RAM_RAME \
+	WEIGHTS_ROTATOR_genblk1_0__BRAM_BRAM_RAMW \
+	WEIGHTS_ROTATOR_genblk1_1__BRAM_BRAM_RAMW } -pattern {{name : sram_pg_mesh} {nets : {VDD VSS}}}
 compile_pg -strategies {sram_ring sram_pg_mesh}
 
 # Create Outer core ring
 create_pg_ring_pattern ring_pattern -horizontal_layer M12 -horizontal_width {5} -horizontal_spacing {2} -vertical_layer M13 -vertical_width {5} -vertical_spacing {2}
-set_pg_strategy core_ring -pattern {{name:ring_pattern} {nets: {VDD VSS}}{offset: {3 3}}} -core
+set_pg_strategy core_ring -pattern {{name:ring_pattern} {nets: {VDD VSS}} {offset: {3 3}}} -core
 compile_pg -strategies {core_ring}
 
 # Create std cell rails 
 create_pg_std_cell_conn_pattern std_rail_pattern -layers M0 -check_std_cell_drc true
-set_pg_strategy std_pwr_rail -pattern {{name: std_rail_pattern}{nets: VDD VSS}} -extension {{{stop : outermost_ring}}} -design_boundary
+set_pg_strategy std_pwr_rail -pattern {{name: std_rail_pattern}{nets: VDD VSS}} -core -extension {{{stop : first_target}}} -blockage {macros_with_keepout : PIXELS_RAM_RAME \
+	WEIGHTS_ROTATOR_genblk1_0__BRAM_BRAM_RAMW \
+	WEIGHTS_ROTATOR_genblk1_1__BRAM_BRAM_RAMW }
 compile_pg -strategies {std_pwr_rail}
 
 # Create Power Straps
-create_pg_mesh_pattern power_straps_horizon -layers {{vertical_layer : M13} {width : 1} {spacing : interleaving} {pitch : 30} {trim : false}}
-set_pg_strategy Verticl_Straps -pattern {{name: power_straps_horizon}{nets: VDD VSS}} -extension {{{stop : outermost_ring}}} -design_boundary
+create_pg_mesh_pattern power_straps_horizon -layers {{vertical_layer : M13} {width : 1} {spacing : interleaving} {pitch : 30} {offset : 5}}
+set_pg_strategy Verticl_Straps -pattern {{name: power_straps_horizon}{nets: VDD VSS}} -core -extension {{{stop : first_target}}}
 
-create_pg_mesh_pattern power_straps_verti -layers {{horizontal_layer : M12} {width : 1} {spacing : interleaving} {pitch : 30} {trim : false}}
-set_pg_strategy Horizon_Straps -pattern {{name: power_straps_verti}{nets: VDD VSS}} -extension {{{stop : outermost_ring}}} -design_boundary
+create_pg_mesh_pattern power_straps_verti -layers {{horizontal_layer : M12} {width : 1} {spacing : interleaving} {pitch : 30} {offset : 5}}
+set_pg_strategy Horizon_Straps -pattern {{name: power_straps_verti}{nets: VDD VSS}} -core -extension {{{stop : first_target}}}
 
 compile_pg -strategies {Verticl_Straps Horizon_Straps}
 
