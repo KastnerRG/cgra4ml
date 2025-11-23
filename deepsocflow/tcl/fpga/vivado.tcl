d9f45757590a117273011f91ca6a7314bdcee693
abarajithan11 <abarajithan07@gmail.com>
2025-11-23 12:02:13 -0800

    Generalize tcl for pynq and zcu104

--- deepsocflow/tcl/fpga/vivado.tcl
+++ deepsocflow/tcl/fpga/vivado.tcl
@@ -5,17 +5,31 @@ update_compile_order -fileset sources_1
 set_property top axi_cgra4ml [current_fileset]
 create_bd_cell -type module -reference axi_cgra4ml axi_cgra4ml_0
 
-# Connect full AXI ports
-connect_bd_intf_net [get_bd_intf_pins axi_cgra4ml_0/m_axi_output] [get_bd_intf_pins $PS_S_AXI_OUTPUT]
-connect_bd_intf_net [get_bd_intf_pins axi_cgra4ml_0/m_axi_pixel] [get_bd_intf_pins $PS_S_AXI_PIXELS]
-connect_bd_intf_net [get_bd_intf_pins axi_cgra4ml_0/m_axi_weights] [get_bd_intf_pins $PS_S_AXI_WEIGHTS]
-
 # Connect AXI-lite port with automation
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {Auto} Clk_slave {Auto} Clk_xbar {Auto} Master {/zynq_ultra_ps_e_0/M_AXI_HPM1_FPD} Slave {/axi_cgra4ml_0/s_axil} ddr_seg {Auto} intc_ip {New AXI Interconnect} master_apm {0}}  [get_bd_intf_pins axi_cgra4ml_0/s_axil]
-# Clk automations
-apply_bd_automation -rule xilinx.com:bd_rule:clkrst -config { Clk {$PS_CLK (250 MHz)} Freq {100} Ref_Clk0 {} Ref_Clk1 {} Ref_Clk2 {}}  [get_bd_pins zynq_ultra_ps_e_0/saxihp0_fpd_aclk]
-apply_bd_automation -rule xilinx.com:bd_rule:clkrst -config { Clk {$PS_CLK (250 MHz)} Freq {100} Ref_Clk0 {} Ref_Clk1 {} Ref_Clk2 {}}  [get_bd_pins zynq_ultra_ps_e_0/saxihpc0_fpd_aclk]
-apply_bd_automation -rule xilinx.com:bd_rule:clkrst -config { Clk {$PS_CLK (250 MHz)} Freq {100} Ref_Clk0 {} Ref_Clk1 {} Ref_Clk2 {}}  [get_bd_pins zynq_ultra_ps_e_0/saxihpc1_fpd_aclk]
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config " Clk_master {Auto} Clk_slave {Auto} Clk_xbar {Auto} Master $PS_M_AXI_LITE Slave {/axi_cgra4ml_0/s_axil} ddr_seg {Auto} intc_ip {New AXI SmartConnect} master_apm {0}"  [get_bd_intf_pins axi_cgra4ml_0/s_axil]
+
+# Connect full AXI ports. Try connecting directly first, if that fails use automation
+if {[catch {
+  connect_bd_intf_net [get_bd_intf_pins axi_cgra4ml_0/m_axi_output] [get_bd_intf_pins $PS_S_AXI_OUTPUT]
+  apply_bd_automation -rule xilinx.com:bd_rule:clkrst -config "Clk {$PS_CLK (250 MHz)} Freq {100} Ref_Clk0 {} Ref_Clk1 {} Ref_Clk2 {}"  [get_bd_pins $CLK_OUTPUT]
+} errmsg]} {
+  puts "INFO: Direct connection for m_axi_output failed, using automation: $errmsg"
+  apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config " Clk_master {$PS_CLK (150 MHz)} Clk_slave {Auto} Clk_xbar {Auto} Master {/axi_cgra4ml_0/m_axi_output} Slave $PS_S_AXI_OUTPUT ddr_seg {Auto} intc_ip {New AXI Interconnect} master_apm {0}"  [get_bd_intf_pins $PS_S_AXI_OUTPUT]
+}
+if {[catch {
+  connect_bd_intf_net [get_bd_intf_pins axi_cgra4ml_0/m_axi_pixel] [get_bd_intf_pins $PS_S_AXI_PIXELS]
+  apply_bd_automation -rule xilinx.com:bd_rule:clkrst -config "Clk {$PS_CLK (250 MHz)} Freq {100} Ref_Clk0 {} Ref_Clk1 {} Ref_Clk2 {}"  [get_bd_pins $CLK_PIXELS]
+} errmsg]} {
+  puts "INFO: Direct connection for m_axi_pixel failed, using automation: $errmsg"
+  apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config " Clk_master {$PS_CLK (150 MHz)} Clk_slave {Auto} Clk_xbar {Auto} Master {/axi_cgra4ml_0/m_axi_pixel} Slave $PS_S_AXI_PIXELS ddr_seg {Auto} intc_ip {New AXI Interconnect} master_apm {0}"  [get_bd_intf_pins $PS_S_AXI_PIXELS]
+}
+if {[catch {
+  connect_bd_intf_net [get_bd_intf_pins axi_cgra4ml_0/m_axi_weights] [get_bd_intf_pins $PS_S_AXI_WEIGHTS]
+  apply_bd_automation -rule xilinx.com:bd_rule:clkrst -config "Clk {$PS_CLK (250 MHz)} Freq {100} Ref_Clk0 {} Ref_Clk1 {} Ref_Clk2 {}"  [get_bd_pins $CLK_WEIGHTS]
+} errmsg]} {
+  puts "INFO: Direct connection for m_axi_weights failed, using automation: $errmsg"
+  apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config " Clk_master {$PS_CLK (150 MHz)} Clk_slave {Auto} Clk_xbar {Auto} Master {/axi_cgra4ml_0/m_axi_weights} Slave $PS_S_AXI_WEIGHTS ddr_seg {Auto} intc_ip {New AXI Interconnect} master_apm {0}"  [get_bd_intf_pins $PS_S_AXI_WEIGHTS]
+}
 
 generate_target all [get_files ./${PROJECT_NAME}/${PROJECT_NAME}.srcs/sources_1/bd/design_1/design_1.bd]
 make_wrapper -files [get_files ./${PROJECT_NAME}/${PROJECT_NAME}.srcs/sources_1/bd/design_1/design_1.bd] -top
@@ -24,8 +38,8 @@ set_property top design_1_wrapper [current_fileset]
 update_compile_order -fileset sources_1
 
 # Set AXl-lite and full_AXI addresses
-set_property range 256M [get_bd_addr_segs {zynq_ultra_ps_e_0/Data/SEG_axi_cgra4ml_0_reg0}]
-set_property offset ${CONFIG_BASEADDR} [get_bd_addr_segs {zynq_ultra_ps_e_0/Data/SEG_axi_cgra4ml_0_reg0}]
+set_property range 256M [get_bd_addr_segs $ZYNQ_PS/Data/SEG_axi_cgra4ml_0_reg0]
+set_property offset ${CONFIG_BASEADDR} [get_bd_addr_segs $ZYNQ_PS/Data/SEG_axi_cgra4ml_0_reg0]
 assign_bd_address
 save_bd_design
 
