33519d982d21712d495901346d3215fd114a026e
abarajithan11 <abarajithan07@gmail.com>
2026-01-05 23:58:27 -0800

    Minor changes to actions

--- run/work/vivado_flow.tcl
+++
@@ -1,8 +0,0 @@
-
-set PROJECT_NAME dsf_zcu104
-set RTL_DIR      /home/a.gnaneswaran/cgra4ml-ibex/deepsocflow/rtl
-set CONFIG_DIR   .
-
-source config_hw.tcl
-source /home/a.gnaneswaran/cgra4ml-ibex/deepsocflow/tcl/fpga/zcu104.tcl
-source /home/a.gnaneswaran/cgra4ml-ibex/deepsocflow/tcl/fpga/vivado.tcl
