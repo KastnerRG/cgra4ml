f970d5606508d101c4348e60aec2c7861657793c
JDRadatti <jdradatti@gmail.com>
2026-06-01 07:57:54 -0700

    Update local paths in run/vivado_flow.tcl

--- run/vivado_flow.tcl
+++ run/vivado_flow.tcl
@@ -3,6 +3,6 @@ set PROJECT_NAME dsf_zcu104
 set RTL_DIR      [file normalize [file join [file dirname [info script]] ".." "deepsocflow" "rtl"]]
 set CONFIG_DIR   [file normalize [file join [file dirname [info script]] "work"]]
 
-source [file normalize [file join [file dirname [info script]] ".." "config_hw.tcl"]]
+source [file normalize [file join $CONFIG_DIR "config_hw.tcl"]]
 source [file normalize [file join [file dirname [info script]] ".." "deepsocflow" "tcl" "fpga" "zcu104.tcl"]]
 source [file normalize [file join [file dirname [info script]] ".." "deepsocflow" "tcl" "fpga" "vivado.tcl"]]
